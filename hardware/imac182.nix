fe3bc65223b5367f5ff308dd24e1f1e1b1b5ddaf
Paepcke, Michael <git@paepcke.de>
2024-06-19 08:49:36 +0200

    update

---
+++ hardware/imac182.nix
@@ -0,0 +1,25 @@
+{
+  description = "flake for nixmac182 [ apple imac18,2 ]";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    nixos-hardware,
+  }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nixos-hardware.nixosModules.apple-imac-18-2
+          ./hardware-configuration.nix
+          ./configuration.nix
+          ./feature/virt.nix
+          ./hardware/macfix.nix
+        ];
+      };
+    };
+  };
+}
