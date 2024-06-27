51246be12555372f6901b86acc777c29056d0bd9
Paepcke, Michael <git@paepcke.de>
2024-06-27 09:57:22 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -24,16 +24,16 @@
     };
   };
 
-  # boot.kernelPatches = [
-  #  {
-  #    name = "bcrm-config";
-  #    patch = null;
-  #    extraConfig = ''
-  #      BT_HCIUART_BCM y
-  #      SND_HDA_CODEC_CS8409 y
-  #    '';
-  #  }
-  # ];
+  boot.kernelPatches = [
+    {
+      name = "bcrm-config";
+      patch = null;
+      extraConfig = ''
+        BT_HCIUART_BCM y
+        SND_HDA_CODEC_CS8409 y
+      '';
+    }
+  ];
 
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
