3f19a063b7f3ab7dea4532031b77a9c8291b5020
Paepcke, Michael <git@paepcke.de>
2024-07-02 12:33:10 +0200

    update

--- flake.nix
+++ flake.nix
@@ -37,14 +37,14 @@
         system = "x86_64-linux";
         modules = [
           "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-base.nix"
-          nixos-hardware.nixosModules.apple-macbook-pro-14-1
-          home-manager.nixosModules.home-manager
-          ./hardware-configuration.nix
-          ./hardware/kb-uk.nix
-          ./configuration.nix
-          ./roles/desktop.nix
-          ./modules/virtual.nix
-          {networking.hostName = "nixbook141";}
+          # nixos-hardware.nixosModules.apple-macbook-pro-14-1
+          # home-manager.nixosModules.home-manager
+          # ./hardware-configuration.nix
+          # ./hardware/kb-uk.nix
+          # ./configuration.nix
+          # ./roles/desktop.nix
+          # ./modules/virtual.nix
+          # {networking.hostName = "nixbook141";}
         ];
       };
       nixbook141-console = nixpkgs.lib.nixosSystem {
