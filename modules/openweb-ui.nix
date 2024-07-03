f994cde9c7ba9a6c0dfff57c4d40003a3ec5ce4e
Paepcke, Michael <git@paepcke.de>
2024-07-03 22:00:10 +0200

    update

--- modules/openweb-ui.nix
+++ modules/openweb-ui.nix
@@ -6,11 +6,9 @@
     open-webui = {
       enable = true;
       environment = ''
-        {
         ANONYMIZED_TELEMETRY = "False";
         DO_NOT_TRACK = "True";
         SCARF_NO_ANALYTICS = "True";
-        }
       '';
       host = "127.0.0.1";
       port = 8080;
