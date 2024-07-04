0624e9878c7e2d7981b198d5829f0eda89b4b669
Paepcke, Michael <git@paepcke.de>
2024-07-04 12:02:25 +0200

    update

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -39,6 +39,7 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.virbr0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
