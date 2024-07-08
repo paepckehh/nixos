c1faf9badc9408aafd2a7c676eff0d01d6c46825
PAEPCKE, Michael <git@paepcke.de>
2024-07-08 19:15:49 +0200

    update

--- modules/bootlogo.nix
+++ modules/bootlogo.nix
@@ -3,14 +3,16 @@
   lib,
   pkgs,
   ...
-}: let
-  nixos-boot-src = pkgs.fetchFromGitHub {
+}:
+
+nixos-boot-src = pkgs.fetchFromGitHub {
     owner = "Melkor333";
     repo = "nixos-boot";
     rev = "main";
     sha256 = "sha256-Dj8LhVTOrHEnqgONbCEKIEyglO7zQej+KS08faO9NJk=";
-  };
-in {
+};
+
+{
   imports = ["${nixos-boot-src}/modules.nix"];
   nixos-boot.enable = true;
 }
