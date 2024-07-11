f22afc2c65245d65b7cb0612148d3a0fc23e8c52
PAEPCKE, Michael <git@paepcke.de>
2024-07-11 08:26:15 +0200

    update

--- server/adguard.nix
+++ server/adguard.nix
@@ -3,14 +3,16 @@
   #-=# SERVICES #=-#
   ##################
   services.adguardhome = {
-    enabled = true;
+    enable = true;
     allowDHCP = false;
     host = "127.0.0.1";
     port = "3000";
     mutableSettings = true;
     openFirewall = false;
     settings = {
-      http.address = "127.0.0.1:3000";
+      http = {
+        address = "127.0.0.1:3000";
+      };
       dns = {
         bind_hosts = ["127.0.0.1"];
         bind_port = 5353;
