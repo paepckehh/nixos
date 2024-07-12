6ae521b99bcfe94b7b9ba4c15fd530275b2f2462
PAEPCKE, Michael <git@paepcke.de>
2024-07-12 21:33:19 +0200

    update

--- modules/chronyPublic.nix
+++ modules/chronyPublic.nix
@@ -9,13 +9,13 @@
       "ntp.3eck.net"
       "ntp.trifence.ch"
       "ntp.zeitgitter.net"
-      "mmo1.nts.netnod.se"
-      "mmo2.nts.netnod.se"
-      "sth1.nts.netnod.se"
-      "sth2.nts.netnod.se"
-      "svl1.nts.netnod.se"
-      "svl2.nts.netnod.se"
       "paris.time.system76.com"
+      "brazil.time.system76.com"
+      "ohio.time.system76.com"
+      "oregon.time.system76.com"
+      "virginia.time.system76.com"
+      "stratum1.time.cifelli.xyz"
+      "time.txryan.com"
     ];
   };
 }
