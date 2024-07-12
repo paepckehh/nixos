40ea668c05c975755a04309fac2d3db66f553aaa
PAEPCKE, Michael <git@paepcke.de>
2024-07-12 11:01:58 +0200

    update

--- modules/securepublictime.nix
+++ modules/securepublictime.nix
@@ -6,6 +6,15 @@
       "ntppool1.time.nl"
       "ntppool2.time.nl"
       "nts.netnod.se"
+      "ntp.3eck.net"
+      "ntp.trifence.ch"
+      "ntp.zeitgitter.net"
+      "mmo1.nts.netnod.se"
+      "mmo2.nts.netnod.se"
+      "sth1.nts.netnod.se"
+      "sth2.nts.netnod.se"
+      "svl1.nts.netnod.se"
+      "svl2.nts.netnod.se"
     ];
   };
 }
