08c91799ce0b6c2636f9658b341264aab155b7b4
PAEPCKE, Michael <git@github.com>
2024-07-19 09:32:29 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -79,7 +79,6 @@
     blacklistedKernelModules = ["ax25" "netrom" "rose" "affs" "bfs" "befs" "freevxfs" "f2fs" "hpfs" "jfs" "minix" "nilfs2" "omfs" "qnx4" "qnx6" "sysv"];
     kernelPackages = pkgs.linuxPackages_latest; # opt _hardened
     kernelParams = ["slab_nomerge" "page_poison=1" "page_alloc.shuffle=1" "debugfs=off" "ipv6.disable=1" "hid_apple.iso_layout=0"];
-    # kernelParams = ["slab_nomerge" "page_poison=1" "page_alloc.shuffle=1" "debugfs=off" "ipv6.disable=1" "hid_apple.iso_layout=0" "i915.enable_guc=3"];
     kernelModules = ["acpi_call" "kvm-intel" "kvm-amd" "vfat" "exfat"];
     readOnlyNixStore = lib.mkForce true;
     initrd = {
@@ -145,8 +144,7 @@
   hardware = {
     cpu.intel.updateMicrocode = lib.mkForce true;
     enableRedistributableFirmware = lib.mkForce true;
-    # graphics.extraPackages = with pkgs; [intel-vaapi-driver intel-media-driver];
-    # graphics.extraPackages = with pkgs; [intel-vaapi-driver];
+    graphics.extraPackages = with pkgs; [intel-vaapi-driver intel-media-driver];
     facetimehd.enable = lib.mkForce false;
   };
   zramSwap = {
@@ -172,7 +170,7 @@
   #-=# SECURITY #=-#
   ##################
   security = {
-    auditd.enable = true;
+    # auditd.enable = true;
     allowSimultaneousMultithreading = true; # xxx
     lockKernelModules = lib.mkForce true;
     protectKernelImage = lib.mkForce true;
