ea8669d1353cd87ca904f23b99d61d8b66a4701e
PAEPCKE, Michael <git@github.com>
2024-07-19 08:01:48 +0200

    update

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -12,7 +12,7 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "uas" "usb_storage" "sd_mod"];
+  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "uas" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
