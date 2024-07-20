49d7f81a8dfcab6e0d084a6e84d91a90de9b0eeb
PAEPCKE, Michael <git@github.com>
2024-07-20 10:39:33 +0200

    update

--- person/mp.nix
+++ person/mp.nix
@@ -8,7 +8,6 @@
   #-=# IMPORTS #=-#
   #################
   imports = [
-    ./luks.nix
     ../user/me.nix
   ];
 
@@ -37,6 +36,29 @@
             userEmail = lib.mkForce "git@github.com";
           };
         };
+      };
+    };
+  };
+
+  #####################
+  #-=# FILESYSTEMS #=-#
+  #####################
+  fileSystems = {
+    "/" = lib.mkForce {
+      device = "/dev/disk/by-uuid/783b1348-9349-494a-819f-5dd80eb0976d";
+      fsType = "ext4";
+    };
+  };
+
+  ##############
+  #-=# BOOT #=-#
+  ##############
+  boot = {
+    initrd = {
+      availableKernelModules = ["aesni_intel" "cryptd"];
+      luks = {
+        mitigateDMAAttacks = lib.mkForce true;
+        devices."luks-d23b5430-fff4-456e-a94f-951fb8ef6992".device = "/dev/disk/by-uuid/d23b5430-fff4-456e-a94f-951fb8ef6992";
       };
     };
   };
