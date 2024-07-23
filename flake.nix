12815c685fac95a4b371679de4fa185b086656ab
PAEPCKE, Michael <git@github.com>
2024-07-23 09:48:19 +0200

    update

--- flake.nix
+++ flake.nix
@@ -10,7 +10,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      # url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
