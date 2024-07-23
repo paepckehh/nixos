af4a680a94f9cde8273ed84f6f9bb4407181ceec
PAEPCKE, Michael <git@github.com>
2024-07-23 08:44:22 +0200

    update

--- server/unifi.nix
+++ server/unifi.nix
@@ -11,6 +11,7 @@
       enable = true;
       openFirewall = false;
       unifiPackage = pkgs.unifi8;
+      mongodbPackage = pkgs.mongodb-6_0;
     };
     prometheus.exporters.unifi = {
       enable = false;
