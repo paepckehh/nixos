c1344ccdbd7d4e9a98fa8c679e8d80f4709708a0
PAEPCKE, Michael <git@github.com>
2024-07-23 09:18:53 +0200

    update

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "nixos generic flake";
   inputs = {
     # nixpkgs.url = "github:NixOS/nixpkgs/master";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
