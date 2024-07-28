a34816d90ea3241495dbccdac0ec618df390f8fa
PAEPCKE, Michael <git@github.com>
2024-07-28 06:34:12 +0200

    update

--- flake.nix
+++ flake.nix
@@ -112,7 +112,7 @@
           ./desktop/gnome.nix
           ./person/desktop/mp.nix
           ./server/adguard.nix
-          # ./server/unifi.nix
+          ./server/unifi.nix
           {networking.hostName = "iss-mp";}
         ];
       };
