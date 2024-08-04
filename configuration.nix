b80706f0b3cb032e5ce37596108354796341ad20
PAEPCKE, Michael <git@github.com>
2024-08-04 21:29:34 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -37,16 +37,19 @@
       require-sigs = lib.mkForce true;
       preallocate-contents = true;
       allowed-uris = lib.mkForce [
-        "https://nix-community.cachix.org"
         "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://nixpkgs-unfree.cachix.org"
       ];
       substituters = lib.mkForce [
-        "https://nix-community.cachix.org"
         "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+        "https://nixpkgs-unfree.cachix.org"
       ];
       trusted-public-keys = lib.mkForce [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
       ];
     };
     gc = {
