a3f2dd7b96d9780deb7214af4e1da225ee4c2438
PAEPCKE, Michael <git@github.com>
2024-08-04 11:17:23 +0200

    update

--- modules/useBuilder.nix
+++ modules/useBuilder.nix
@@ -8,21 +8,23 @@
   #-=# NIX #=-#
   #############
   nix = {
-    allowed-uris = lib.mkForce [
-      "http://nix-build.lan"
-      "https://nix-community.cachix.org"
-      "https://cache.nixos.org"
-    ];
-    substituters = lib.mkForce [
-      "http://nix-build.lan"
-      "https://nix-community.cachix.org"
-      "https://cache.nixos.org/"
-    ];
-    trusted-public-keys = lib.mkForce [
-      "nix-build.lan-1:uQdcz6CZnERTINBerLtn4+xfJuK1KenGkco7YFxiDJA="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+    settings = {
+      allowed-uris = lib.mkForce [
+        "http://nix-build.lan"
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
+      ];
+      substituters = lib.mkForce [
+        "http://nix-build.lan"
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = lib.mkForce [
+        "nix-build.lan-1:uQdcz6CZnERTINBerLtn4+xfJuK1KenGkco7YFxiDJA="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
     distributedBuilds = true;
     buildMachines = [
       {
