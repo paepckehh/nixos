b39966c6f72e4ab7c46477250b59b99b8685935a
PAEPCKE, Michael <git@github.com>
2024-08-04 19:53:12 +0200

    update

--- flake.nix
+++ flake.nix
@@ -53,8 +53,8 @@
           ./desktop/gnome.nix
           ./person/desktop/mp.nix
           ./server/adguard.nix
-          # ./server/openweb-ui.nix
-          # ./server/unifi.nix
+          ./server/openweb-ui.nix
+          ./server/unifi.nix
           ./server/virtual.nix
           {networking.hostName = "nixos-mp";}
         ];
