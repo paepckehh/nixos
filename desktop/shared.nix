9a0eb8089f80a8709bacfced4b72e49371d20694
PAEPCKE, Michael <git@github.com>
2024-08-05 20:47:06 +0200

    update

--- desktop/shared.nix
+++ desktop/shared.nix
@@ -31,7 +31,7 @@
   #-=# ENVIRONMENT #=-#
   #####################
   environment = {
-    systemPackages = with pkgs; [alacritty gparted networkmanagerapplet opensnitch-ui];
+    systemPackages = with pkgs; [alacritty element-desktop gparted networkmanagerapplet opensnitch-ui];
     variables = {
       BROWSER = "librewolf";
       TERMINAL = "alacritty";
