da89cb54b79d169d46065f5c37c8f2034beea69b
PAEPCKE, Michael <git@github.com>
2024-08-05 08:00:21 +0200

    update

--- server/unifi.nix
+++ server/unifi.nix
@@ -18,4 +18,20 @@
       port = 9130;
     };
   };
+  static-web-server = {
+    enable = true;
+    listen = "[::]:80";
+    root = "/var/www";
+    configuration = {
+      general = {
+        directory-listing = true;
+      };
+    };
+  };
+  #################
+  #-=# SYSTEMD #=-#
+  #################
+  systemd.tmpfiles.rules = [
+    "d /var/www 0755 root users"
+  ];
 }
