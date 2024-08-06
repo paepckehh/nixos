f80a702477c1371d8899cfd0c588e08fb2cc2be7
PAEPCKE, Michael <git@github.com>
2024-08-06 08:38:18 +0200

    update

--- flake.nix
+++ flake.nix
@@ -15,25 +15,6 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs-Release";
     };
-    nixpkgs.overlays = [
-      (self: super: {
-        ccacheWrapper = super.ccacheWrapper.override {
-          extraConfig = ''
-            export CCACHE_COMPRESS=1
-            export CCACHE_DIR="/var/cache/ccache"
-            export CCACHE_UMASK=007
-            if [ ! -d "$CCACHE_DIR" ]; then
-              echo "Directory '$CCACHE_DIR' does not exist! Please create it with: sudo mkdir -m0770 '$CCACHE_DIR' && sudo chown root:nixbld '$CCACHE_DIR'"
-              exit 1
-            fi
-            if [ ! -w "$CCACHE_DIR" ]; then
-              echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami): Please verify its access permissions"
-              exit 1
-            fi
-          '';
-        };
-      })
-    ];
   };
   outputs = {
     self,
