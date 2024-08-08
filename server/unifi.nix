efbc7a4f91c7e5acbdd81da00cfd0b6043f3b6db
PAEPCKE, Michael <git@github.com>
2024-08-08 21:12:55 +0200

    update

--- server/unifi.nix
+++ server/unifi.nix
@@ -13,9 +13,23 @@
       unifiPackage = pkgs.unifi8;
       mongodbPackage = pkgs.mongodb-6_0;
     };
-    prometheus.exporters.unifi = {
-      enable = false;
-      port = 9130;
+    prometheus = {
+      exporters = {
+        unifi = {
+          enable = false;
+          port = 9130;
+        };
+        unpoller = {
+          enable = false;
+          controllers = [
+            {
+              url = "https://iss.admin.lan";
+              user = "read-only-account";
+              pass = /etc/nixos/iunifi.pass;
+            }
+          ];
+        };
+      };
     };
     static-web-server = {
       enable = true;
