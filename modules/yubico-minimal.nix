0d1d5c991b7b6259498a1a5609fd7533dbd4a39b
PAEPCKE, Michael <git@github.com>
2024-08-11 11:38:03 +0200

    update

--- modules/yubico-minimal.nix
+++ modules/yubico-minimal.nix
@@ -18,21 +18,21 @@
   #-=# SECURITY #=-#
   ##################
 
-  security = {
-    pam = {
-      services = {
-        login.yubicoAuth = false;
-        sudo.yubicoAuth = false;
-      };
-      yubico = {
-        enable = false;
-        challengeResponsePath = "$HOME/.yubico/challenge";
-        control = "optional"; # required or optional
-        debug = true;
-        mode = "challenge-response";
-      };
-    };
-  };
+  # security = {
+  #   pam = {
+  #     services = {
+  #       login.yubicoAuth = false;
+  #       sudo.yubicoAuth = false;
+  #    };
+  #    yubico = {
+  #     enable = false;
+  #    challengeResponsePath = "$HOME/.yubico/challenge";
+  #    control = "optional"; # required or optional
+  #     debug = true;
+  #       mode = "challenge-response";
+  #     };
+  #   };
+  # };
 
   #####################
   #-=# ENVIRONMENT #=-#
@@ -42,40 +42,40 @@
   # libu2f-host
   # yubico-pam
 
-  environment = {
-    systemPackages = with pkgs; [
-      age-plugin-yubikey
-      pcsclite
-      yubioath-flutter
-      yubikey-touch-detector
-      yubikey-manager
-      yubikey-manager-qt
-      yubikey-personalization
-      yubikey-personalization-gui
-    ];
-  };
+  # environment = {
+  #  systemPackages = with pkgs; [
+  # age-plugin-yubikey
+  # pcsclite
+  # yubioath-flutter
+  # yubikey-touch-detector
+  # yubikey-manager
+  # yubikey-manager-qt
+  # yubikey-personalization
+  # yubikey-personalization-gui
+  #   ];
+  # };
 
   ##################
   #-=# SERVICES #=-#
   ##################
 
   services = {
-    pcscd.enable = true;
-    udev.extraRules = ''
-      ACTION=="remove",\
-      ENV{ID_BUS}=="usb",\
-      ENV{ID_MODEL_ID}=="0407",\
-      ENV{ID_VENDOR_ID}=="1050",\
-      ENV{ID_VENDOR}=="Yubico",\
-      RUN+="${pkgs.systemd}/bin/loginctl lock-sessions" '';
+    udev.packages = [pkgs.yubikey-personalization];
+    # pcscd.enable = false;
+    # udev.extraRules = ''
+    #   ACTION=="remove",\
+    #   ENV{ID_BUS}=="usb",\
+    #   ENV{ID_MODEL_ID}=="0407",\
+    #   ENV{ID_VENDOR_ID}=="1050",\
+    #   ENV{ID_VENDOR}=="Yubico",\
+    #   RUN+="${pkgs.systemd}/bin/loginctl lock-sessions" '';
   };
 
   ##################
   #-=# PROGRAMS #=-#
   ##################
-
-  programs.gnupg.agent = {
-    enable = false;
-    enableSSHSupport = false;
-  };
+  # programs.gnupg.agent = {
+  #   enable = false;
+  #   enableSSHSupport = false;
+  # };
 }
