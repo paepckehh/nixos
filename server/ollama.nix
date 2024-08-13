2c86237c239824f15d26813375fe02a1499501be
PAEPCKE, Michael <git@github.com>
2024-08-13 07:53:18 +0200

    update

--- server/ollama.nix
+++ server/ollama.nix
@@ -8,11 +8,14 @@
       host = "127.0.0.1";
       port = 11434;
       openFirewall = false;
+      user = "ollama";
+      group = "ollama";
+      # acceleration = false;
       # environmentVariables = [];
       # home = "";
+      # models = "";
       # loadModels = [];
       # rocmOverrideGfx = "";
-      # writeablePath = [];
     };
   };
 }
