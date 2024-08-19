b0dbf355d603653f0ab1c01deddfb4f4a5a5ec1b
PAEPCKE, Michael <git@github.com>
2024-08-19 06:56:24 +0200

    update

--- flake.nix
+++ flake.nix
@@ -170,7 +170,7 @@
             networking = {
               hostName = "iss-command-tk";
               domain = "admin.lan";
-              wireless.enable = lib.mkForce false;
+              wireless.enable = false;
             };
           }
         ];
@@ -186,7 +186,7 @@
             networking = {
               hostName = "iss-command-jk";
               domain = "admin.lan";
-              wireless.enable = lib.mkForce false;
+              wireless.enable = false;
             };
           }
         ];
