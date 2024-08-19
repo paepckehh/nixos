1d40ffc20ad500adc820af4c511bf4e1a2f821df
PAEPCKE, Michael <git@github.com>
2024-08-19 06:42:18 +0200

    update

--- desktop/gnome.nix
+++ desktop/gnome.nix
@@ -62,11 +62,11 @@
       games.enable = lib.mkForce false;
       gnome-browser-connector.enable = lib.mkForce false;
       gnome-initial-setup.enable = lib.mkForce false;
-      gnome-keyring.enable = lib.mkForce false;
       gnome-online-accounts.enable = lib.mkForce false;
       gnome-online-miners.enable = lib.mkForce false;
       gnome-remote-desktop.enable = lib.mkForce false;
       gnome-user-share.enable = lib.mkForce false;
+      # gnome-keyring.enable = lib.mkForce false;
     };
     xserver = {
       displayManager.gdm = {
