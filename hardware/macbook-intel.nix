5e819f63801274c2965e71024f529cb7a0f45a78
PAEPCKE, Michael <git@github.com>
2024-09-08 08:06:14 +0000

    update

--- hardware/macbook-intel.nix
+++ hardware/macbook-intel.nix
@@ -27,10 +27,10 @@
     # kernelParams = ["brcmfmac.feature_disable=0x82000"];
     # kernelParams = ["hid_apple.iso_layout=0"];
     # blacklistedKernelModules = ["b43" "bcma" "brcmfmac" "brcmsmac" "ssb"];
+    # extraModulePackages = with config.boot.kernelPackages; [broadcom_sta];
     # kernelModules = ["kvm-intel"];
-    blacklistedKernelModules = ["b43" "bcma" "brcmfmac" "ssb"];
-    kernelParams = ["hid_apple.swap_opt_cmd=1" "intel_iommu=on"];
-    extraModulePackages = with config.boot.kernelPackages; [broadcom_sta];
+    blacklistedKernelModules = ["b43" "bcma" "brcmsmac" "ssb"];
+    kernelParams = ["hid_apple.swap_opt_cmd=1" "hid_apple.iso_layout=0" "intel_iommu=on"];
     initrd = {
       availableKernelModules = [
         "applespi"
@@ -60,9 +60,9 @@
   ####################
   #-=# NETWORKING #=-#
   ####################
-  networking = {
-    # enableB43Firmware = true;
-  };
+  # networking = {
+  #  enableB43Firmware = true;
+  #};
 
   ##################
   #-=# SERVICES #=-#
