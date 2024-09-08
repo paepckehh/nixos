c9ee9134d9d728ba28e2bef5f44a0f78bb9126b4
PAEPCKE, Michael <git@github.com>
2024-09-08 07:34:56 +0000

    update

--- hardware/macbook-intel.nix
+++ hardware/macbook-intel.nix
@@ -26,7 +26,7 @@
   boot = {
     # kernelParams = ["brcmfmac.feature_disable=0x82000"];
     # kernelParams = ["hid_apple.iso_layout=0"];
-    blacklistedKernelModules = ["b43" "brcma" "brcmfmac" "brcmsmac" "ssb"];
+    blacklistedKernelModules = ["b43" "brcma" "brcmsmac" "ssb"];
     kernelParams = ["hid_apple.swap_opt_cmd=1"];
     # kernelModules = ["kvm-intel" "wl"];
     kernelModules = ["kvm-intel" "brcmfmac"];
