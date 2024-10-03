b3ed77e8739bc246f28d1bcc7e689898518797d4
PAEPCKE, Michael <git@paepcke.de>
2024-10-03 05:44:33 +0000

    update

--- flake.nix
+++ flake.nix
@@ -75,19 +75,6 @@
           {networking.hostName = "nixos-console";}
         ];
       };
-      nixos-console-mp = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          home-manager.nixosModules.home-manager
-          ./configuration.nix
-          ./hardware/nvidia-off.nix
-          ./hardware/macbook-intel.nix
-          ./modules/chronyPublic.nix
-          ./server/adguard.nix
-          ./person/mpaepcke.nix
-          {networking.hostName = "nixos-console-mp";}
-        ];
-      };
       nixbuilder = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
@@ -98,61 +85,12 @@
           ./desktop/gnome.nix
           ./user/desktop/me.nix
           ./server/builder.nix
+          ./server/virtual.nix
           # ./server/adguard.nix
           # ./server/gitea.nix
           # ./server/ollama.nix
           # ./server/openweb-ui.nix
           {networking.hostName = "nixbuilder";}
-        ];
-      };
-      iss = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          home-manager.nixosModules.home-manager
-          ./configuration.nix
-          ./server/unifi.nix
-          {
-            networking = {
-              hostName = "iss";
-              domain = "admin.lan";
-              wireless.enable = false;
-            };
-            hardware.bluetooth.enable = false;
-          }
-        ];
-      };
-      iss-command-tk = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          home-manager.nixosModules.home-manager
-          ./configuration.nix
-          ./desktop/gnome.nix
-          ./user/desktop/me.nix
-          {
-            networking = {
-              hostName = "iss-command-tk";
-              domain = "admin.lan";
-              wireless.enable = false;
-            };
-            hardware.bluetooth.enable = false;
-          }
-        ];
-      };
-      iss-command-jk = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          home-manager.nixosModules.home-manager
-          ./configuration.nix
-          ./desktop/gnome.nix
-          ./user/desktop/me.nix
-          {
-            networking = {
-              hostName = "iss-command-jk";
-              domain = "admin.lan";
-              wireless.enable = false;
-            };
-            hardware.bluetooth.enable = false;
-          }
         ];
       };
       ########################
