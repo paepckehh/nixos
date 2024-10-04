7e357e386c9d7da25616a615454bcac4e1a05f0b
PAEPCKE, Michael <git@paepcke.de>
2024-10-04 08:07:45 +0000

    update

--- server/webserver-nginx.nix
+++ server/webserver-nginx.nix
@@ -17,6 +17,7 @@
       ];
       defaultHTTPListenPort = 8282;
       virtualHosts = {
+        "/" = {root = "/var/www/catchall";};
         "pki.paepcke.de" = {root = "/var/www/pki.paepcke.de";};
         "it-portal.paepcke.de" = {root = "/var/www.it-portal.paepcke.de";};
       };
