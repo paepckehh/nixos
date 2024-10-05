ef2e4b2da36b33fd3760ea9e2f4bbf510af21371
PAEPCKE, Michael <git@paepcke.de>
2024-10-05 06:33:03 +0000

    update

--- modules/prometheus-node.nix
+++ modules/prometheus-node.nix
@@ -13,11 +13,9 @@
       enabledCollectors = [
         "logind"
         "systemd"
-        "wireguard"
       ];
       disabledCollectors = [];
       openFirewall = true;
-      # firewallFilter = "-i br0 -p tcp -m tcp --dport 9100";
     };
   };
 }
