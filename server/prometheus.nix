aba6b73a86103a65833ab6fbc15fac505f22b9a8
PAEPCKE, Michael <git@paepcke.de>
2024-10-06 16:51:02 +0000

    update

--- server/prometheus.nix
+++ server/prometheus.nix
@@ -54,6 +54,10 @@
         };
       };
     };
+    loki = {
+      enable = true;
+      configFile = "./resources/loki.yaml";
+    };
     promtail = {
       enable = true;
       configuration = {
@@ -66,7 +70,7 @@
         };
         clients = [
           {
-            url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
+            url = "http://127.0.0.1:3100/loki/api/v1/push";
           }
         ];
         scrape_configs = [
@@ -87,68 +91,6 @@
             ];
           }
         ];
-      };
-    };
-    loki = {
-      enable = true;
-      configuration = {
-        server.http_listen_port = 3030;
-        auth_enabled = false;
-        ingester = {
-          lifecycler = {
-            address = "127.0.0.1";
-            ring = {
-              kvstore = {
-                store = "inmemory";
-              };
-              replication_factor = 1;
-            };
-          };
-          chunk_idle_period = "1h";
-          max_chunk_age = "1h";
-          chunk_target_size = 999999;
-          chunk_retain_period = "30s";
-        };
-        schema_config = {
-          configs = [
-            {
-              from = "2022-06-06";
-              store = "boltdb-shipper";
-              object_store = "filesystem";
-              schema = "v11";
-              index = {
-                prefix = "index_";
-                period = "24h";
-              };
-            }
-          ];
-        };
-        storage_config = {
-          boltdb_shipper = {
-            active_index_directory = "/var/lib/loki/boltdb-shipper-active";
-            cache_location = "/var/lib/loki/boltdb-shipper-cache";
-            cache_ttl = "24h";
-          };
-          filesystem = {
-            directory = "/var/lib/loki/chunks";
-          };
-        };
-        limits_config = {
-          reject_old_samples = true;
-          reject_old_samples_max_age = "168h";
-        };
-        table_manager = {
-          retention_deletes_enabled = false;
-          retention_period = "0s";
-        };
-        compactor = {
-          working_directory = "/var/lib/loki";
-          compactor_ring = {
-            kvstore = {
-              store = "inmemory";
-            };
-          };
-        };
       };
     };
   };
