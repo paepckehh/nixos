2ba30af8987d16b069a88f4856f593ceb9046f10
PAEPCKE, Michael <git@paepcke.de>
2024-10-06 16:36:16 +0000

    update

--- server/prometheus.nix
+++ server/prometheus.nix
@@ -108,7 +108,6 @@
           max_chunk_age = "1h";
           chunk_target_size = 999999;
           chunk_retain_period = "30s";
-          max_transfer_retries = 0;
         };
         schema_config = {
           configs = [
