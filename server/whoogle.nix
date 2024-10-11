05204bde00bdbf6b41a3fea735ca2f2a826ed5a5
PAEPCKE, Michael <git@paepcke.de>
2024-10-11 08:40:21 +0000

    update

--- server/whoogle.nix
+++ server/whoogle.nix
@@ -9,9 +9,8 @@
   virtualisation.oci-containers.containers = {
     whoogle = {
       image = "benbusby/whoogle-search";
-      ports = ["0.0.0.0:8080:80"];
+      ports = ["0.0.0.0:8080:8080"];
       environment = {
-        EXPOSE_PORT = "80";
         WHOOGLE_MINIMAL = " 1 ";
         WHOOGLE_RESULTS_PER_PAGE = " 50 ";
         WHOOGLE_CONFIG_LANGUAGE = " en ";
@@ -20,16 +19,16 @@
         WHOOGLE_CONFIG_URL = "http://localhost:8080";
       };
     };
-    baserow = {
-      image = "baserow/baserow";
-      ports = ["0.0.0.0:8181:80"];
-      environment = {
-        BASEROW_PUBLIC_URL = "http://localhost:8181";
-      };
-      volumes = [
-        "baserow_data:/var/baserow/data"
-      ];
-    };
+    # baserow = {
+    #  image = "baserow/baserow";
+    #  ports = ["0.0.0.0:8181:80"];
+    #  environment = {
+    #    BASEROW_PUBLIC_URL = "http://localhost:8181";
+    #  };
+    #  volumes = [
+    #    "baserow_data:/var/baserow/data"
+    #  ];
+    # };
     grist = {
       image = "gristlabs/grist";
       ports = ["0.0.0.0:8282:80"];
