47a41c5437e6ccc6bd0edde01dbe05640bb5f4c1
PAEPCKE, Michael <git@paepcke.de>
2024-10-11 05:22:57 +0000

    update

--- server/whoogle.nix
+++ server/whoogle.nix
@@ -9,7 +9,7 @@
   virtualisation.oci-containers.containers.whoogle = {
     image = "benbusby/whoogle-search";
     ports = ["0.0.0.0:5000:5000"];
-    environment = "
+    environment = [
       HTTPS_ONLY = " 1 ";
       WHOOGLE_MINIMAL = " 1 ";
       WHOOGLE_RESULTS_PER_PAGE = " 50 ";
@@ -17,6 +17,6 @@
       WHOOGLE_CONFIG_SEARCH_LANGUAGE = " en ";
       WHOOGLE_CONFIG_SAFE = " 1 ";
       WHOOGLE_CONFIG_URL = " http://localhost:50000 ";
-    ";
+    ];
   };
 }
