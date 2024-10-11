14391cf1e9cea7602114c548f530ad30cfb783a6
PAEPCKE, Michael <git@paepcke.de>
2024-10-11 08:25:30 +0000

    update

--- server/whoogle.nix
+++ server/whoogle.nix
@@ -9,9 +9,9 @@
   virtualisation.oci-containers.containers = {
     whoogle = {
       image = "benbusby/whoogle-search";
-      ports = ["0.0.0.0:8080:8080"];
+      ports = ["0.0.0.0:8080:80"];
       environment = {
-        EXPOSE_PORT = "8080";
+        EXPOSE_PORT = "80";
         WHOOGLE_MINIMAL = " 1 ";
         WHOOGLE_RESULTS_PER_PAGE = " 50 ";
         WHOOGLE_CONFIG_LANGUAGE = " en ";
@@ -20,18 +20,11 @@
         WHOOGLE_CONFIG_URL = "http://localhost:8080";
       };
     };
-    # nocdb = {
-    #  image = "nocdb/nocdb";
-    #  ports = ["0.0.0.0:8181:8080"];
-    #  environment = {
-    #    EXPOSE_PORT = "8080";
-    #  };
-    # };
     baserow = {
       image = "baserow/baserow";
-      ports = ["0.0.0.0:8282:8080"];
+      ports = ["0.0.0.0:8181:80"];
       environment = {
-        EXPOSE_PORT = "8080";
+        BASEROW_PUBLIC_URL = "http://localhost:8181";
       };
       volumes = [
         "baserow_data:/var/baserow/data"
@@ -39,24 +32,19 @@
     };
     grist = {
       image = "gristlabs/grist";
-      ports = ["0.0.0.0:8383:8080"];
-      environment = {
-        EXPOSE_PORT = "8080";
-      };
+      ports = ["0.0.0.0:8282:80"];
     };
     speedtest = {
       image = "openspeedtest/latest";
-      ports = ["0.0.0.0:8484:8080"];
-      environment = {
-        EXPOSE_PORT = "8080";
-      };
+      ports = ["0.0.0.0:8383:80"];
     };
+    # nocdb = {
+    #  image = "nocdb/nocdb";
+    #  ports = ["0.0.0.0:8484:80"];
+    # };
     #spot = {
     #  image = "yooooomi/your_spotify_server";
     #  ports = ["0.0.0.0:8585:8080"];
-    #  environment = {
-    #    EXPOSE_PORT = "8080";
-    #  };
     #};
   };
 }
