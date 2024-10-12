2be47965580b423f52e580a91d216af9a9c6d387
PAEPCKE, Michael <git@paepcke.de>
2024-10-12 06:06:04 +0000

    update

--- server/whoogle.nix
+++ server/whoogle.nix
@@ -6,45 +6,39 @@
   ########################
   #-=# VIRTUALISATION #=-#
   ########################
-  virtualisation.oci-containers.containers = {
-    whoogle = {
-      image = "benbusby/whoogle-search";
-      ports = ["0.0.0.0:8080:8080"];
-      environment = {
-        EXPOSE_PORT = "8080";
-        WHOOGLE_MINIMAL = " 1 ";
-        WHOOGLE_RESULTS_PER_PAGE = " 50 ";
-        WHOOGLE_CONFIG_LANGUAGE = " en ";
-        WHOOGLE_CONFIG_SEARCH_LANGUAGE = " en ";
-        WHOOGLE_CONFIG_SAFE = " 1 ";
-        WHOOGLE_CONFIG_URL = "http://localhost:8080";
+  virtualisation = {
+    oci-containers = {
+      containers = {
+        whoogle = {
+          image = "benbusby/whoogle-search";
+          ports = ["0.0.0.0:8080:8080"];
+          environment = {
+            EXPOSE_PORT = "8080";
+            WHOOGLE_MINIMAL = "1";
+            WHOOGLE_RESULTS_PER_PAGE = "50";
+            WHOOGLE_CONFIG_LANGUAGE = "en";
+            WHOOGLE_CONFIG_SEARCH_LANGUAGE = "en";
+            WHOOGLE_CONFIG_SAFE = "1";
+            WHOOGLE_CONFIG_URL = "http://localhost:8080";
+          };
+        };
+        speed = {
+          image = "openspeedtest/latest";
+          ports = ["0.0.0.0:8181:3000"];
+        };
+        grist = {
+          image = "gristlabs/grist";
+          ports = ["0.0.0.0:8282:80"];
+        };
+        # nocdb = {
+        #  image = "nocdb/nocdb";
+        #  ports = ["0.0.0.0:8484:80"];
+        # };
+        #spot = {
+        #  image = "yooooomi/your_spotify_server";
+        #  ports = ["0.0.0.0:8585:8080"];
+        #};
       };
     };
-    # baserow = {
-    #  image = "baserow/baserow";
-    #  ports = ["0.0.0.0:8181:80"];
-    #  environment = {
-    #    BASEROW_PUBLIC_URL = "http://localhost:8181";
-    #  };
-    #  volumes = [
-    #    "baserow_data:/var/baserow/data"
-    #  ];
-    # };
-    grist = {
-      image = "gristlabs/grist";
-      ports = ["0.0.0.0:8282:80"];
-    };
-    speedtest = {
-      image = "openspeedtest/latest";
-      ports = ["0.0.0.0:8383:3000"];
-    };
-    # nocdb = {
-    #  image = "nocdb/nocdb";
-    #  ports = ["0.0.0.0:8484:80"];
-    # };
-    #spot = {
-    #  image = "yooooomi/your_spotify_server";
-    #  ports = ["0.0.0.0:8585:8080"];
-    #};
   };
 }
