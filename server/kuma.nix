245d870b027cd0c982667b5b0d582f6d8d63ab9f
PAEPCKE, Michael <git@paepcke.de>
2024-10-12 08:05:06 +0000

    update

--- server/kuma.nix
+++ server/kuma.nix
@@ -11,7 +11,9 @@
       enable = true;
       appriseSupport = false;
       settings = {
-          UPTIME_KUMA_HOST = "0.0.0.0";
-          UPTIME_KUMA_PORT = "4000";
+        UPTIME_KUMA_HOST = "0.0.0.0";
+        UPTIME_KUMA_PORT = "4000";
       };
+    };
   };
+}
