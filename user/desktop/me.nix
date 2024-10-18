9dc9f07575f4a3a43966e352ad255c19582f570d
PAEPCKE, Michael <git@paepcke.de>
2024-10-18 08:39:29 +0000

    update

--- user/desktop/me.nix
+++ user/desktop/me.nix
@@ -15,7 +15,7 @@
   #-=# ENVIRONMENT #=-#
   #####################
   environment = {
-    systemPackages = with pkgs; [gparted mission-center];
+    systemPackages = with pkgs; [gparted mission-center krita];
     variables = {
       BROWSER = "firejail librewolf";
     };
