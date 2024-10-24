90f0726f9aa0b067f0bc8619035951f794b053d2
PAEPCKE, Michael <git@paepcke.de>
2024-10-24 02:32:24 +0000

    update

--- user/me.nix
+++ user/me.nix
@@ -73,7 +73,6 @@
             ".config/starship.toml".source = ./resources/starship/gruvbox-rainbow.toml;
           };
           packages = with pkgs; [
-            opnborg
             asn
             age
             bandwhich
