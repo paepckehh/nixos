7d1d152ea232e57d8832f295f3fc8f20d1fa70af
PAEPCKE, Michael <git@paepcke.de>
2024-10-26 08:37:42 +0000

    update

--- configuration.nix
+++ configuration.nix
@@ -96,7 +96,6 @@
   boot = {
     blacklistedKernelModules = ["ax25" "netrom" "rose" "affs" "bfs" "befs" "freevxfs" "f2fs" "hpfs" "jfs" "minix" "nilfs2" "omfs" "qnx4" "qnx6" "sysv"];
     kernelPackages = pkgs.linuxPackages_latest;
-    # kernelParams = ["page_alloc.shuffle=1" "ipv6.disable=1"];
     kernelParams = ["page_alloc.shuffle=1"];
     kernelModules = ["vfat" "exfat"];
     readOnlyNixStore = lib.mkForce true;
@@ -238,7 +237,6 @@
   ####################
   networking = {
     useDHCP = lib.mkDefault true;
-    # enableIPv6 = lib.mkForce false;
     usePredictableInterfaceNames = true;
     networkmanager.enable = true;
     wireguard.enable = true;
