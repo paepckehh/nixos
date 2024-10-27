cc520e48bdff00d4188940d4ed4c4887b8d05e93
PAEPCKE, Michael <git@paepcke.de>
2024-10-27 09:46:51 +0000

    update

--- server/opnborg.nix
+++ server/opnborg.nix
@@ -9,6 +9,7 @@
         "OPN_APIKEY" = "+RIb6YWNdcDWMMM7W5ZYDkUvP4qx6e1r7e/Lg/Uh3aBH+veuWfKc7UvEELH/lajWtNxkOaOPjWR8uMcD";
         "OPN_APISECRET" = "8VbjM3HKKqQW2ozOe5PTicMXOBVi9jZTSPCGfGrHp8rW6m+TeTxHyZyAI1GjERbuzjmz6jK/usMCWR/p";
         "OPN_TARGETS" = "opn00.lan";
+        "OPN_HTTPD_ENABLE" = "true";
       };
     };
   };
