ca763d74acae4e55c405e27d9c997a492ffa960a
PAEPCKE, Michael <git@paepcke.de>
2024-11-01 05:31:44 +0000

    update

--- server/opnborg-complex.nix
+++ server/opnborg-complex.nix
@@ -8,7 +8,6 @@
       extraOptions = {
         "OPN_APIKEY" = "+RIb6YWNdcDWMMM7W5ZYDkUvP4qx6e1r7e/Lg/Uh3aBH+veuWfKc7UvEELH/lajWtNxkOaOPjWR8uMcD";
         "OPN_APISECRET" = "8VbjM3HKKqQW2ozOe5PTicMXOBVi9jZTSPCGfGrHp8rW6m+TeTxHyZyAI1GjERbuzjmz6jK/usMCWR/p";
-        "OPN_TLSKEYPIN" = "8VbjM3HKKqQW2ozOe5PTicMXOBVi9jZTSPCGfGrHp8rW6m+TeTxHyZyAI1GjERbuzjmz6jK/usMCWR/p";
         "OPN_MASTER" = "opn01.lan:8443";
         "OPN_TARGETS_HOTSTANDBY" = "opn01.lan:8443";
         "OPN_TARGETS_PRODUCTION" = "opn02.lan:8443,opn03.lan:8443";
