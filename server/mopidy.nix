a54ea2893de7f7ebfddab97570b679a1dd5c397e
PAEPCKE, Michael <git@paepcke.de>
2024-11-02 07:22:00 +0000

    update

--- server/mopidy.nix
+++ server/mopidy.nix
@@ -11,5 +11,5 @@
     mopidy = {
       enable = true;
       extensionPackages = with pkgs; [ mopidy-spotify mopidy-iris mopidy-tidal mopidy-tunein mopidy-youtube mopidy-podcast mopidy-soundcloud ]
-  };
+  }; };
 }
