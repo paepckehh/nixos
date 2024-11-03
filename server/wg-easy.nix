e2fbfc35f67244ef7b2ae322f2b7ad9c2019d8f8
PAEPCKE, Michael <git@paepcke.de>
2024-11-03 07:19:04 +0000

    update

--- server/wg-easy.nix
+++ server/wg-easy.nix
@@ -13,7 +13,7 @@
         wg-easy = {
           hostname = "wg-easy";
           image = "ghcr.io/wg-easy/wg-easy";
-          ports = ["0.0.0.0:51820:51820" "0.0.0.0:51821:51821"];
+          ports = ["0.0.0.0:51822:51822" "0.0.0.0:51821:51821"];
           volumes = ["/var/lib/wg-easy:/etc/wireguard"];
           # extraOptions = ["--cap-add NET_ADMIN" "--cap-add SYS_MODULE" "--sysctl 'net.ipv4.conf.all.src_valid_mark=1'" "--sysctl 'net.ipv4.ip_forward=1'"];
           extraOptions = ["--privileged=true"];
@@ -21,7 +21,7 @@
             PASSWORD_HASH = "$2a$12$z8e9VadQr8XmxQvKisrIR.H6h1KwdiBjBKLlXHshla7pt7RFhTOTy"; # start
             PORT = "51821"; # webgui
             LANG = "DE";
-            WG_PORT = "51820";
+            WG_PORT = "51822";
             WG_HOST = "127.0.0.1";
           };
         };
