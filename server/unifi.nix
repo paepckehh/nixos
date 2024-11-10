6aa4c8a287ca6c1e5e0967dc383638c69e0b659b
PAEPCKE, Michael <git@paepcke.de>
2024-11-10 07:35:23 +0000

    update

--- server/unifi.nix
+++ server/unifi.nix
@@ -3,7 +3,6 @@
   pkgs,
   ...
 }: {
-
   ##################
   #-=# SERVICES #=-#
   ##################
@@ -37,55 +36,58 @@
       };
     };
     dhcpd4 = {
-    enable = false;
-    interfaces = [ "vlan1" ];
-    extraConfig = ''
-      option domain-name-servers 10.0.0.30, 10.0.0.30;
-      option subnet-mask 255.255.255.0;
+      enable = false;
+      interfaces = ["vlan1"];
+      extraConfig = ''
+        option domain-name-servers 10.0.0.30, 10.0.0.30;
+        option subnet-mask 255.255.255.0;
 
-      subnet 10.0.0.0 netmask 255.255.255.0 {
-        option broadcast-address 10.0.0.255;
-        option routers 10.0.0.254;
-        interface vlan1;
-        range 10.0.0.150 10.0.0.200;
-      }
-    '';
-  };
-  bind = {
-    enable = false;
-    cacheNetworks = [ "127.0.0.0/24" "10.0.0.0/24"];
-    zones = {
-      "lan" = {
-        master = true;
-        file = pkgs.writeText "lan" ''
-          $ORIGIN lan.
-          $TTL    1h
-          @            IN      SOA     ns1 hostmaster (
-                                           1    ; Serial
-                                           3h   ; Refresh
-                                           1h   ; Retry
-                                           1w   ; Expire
-                                           1h)  ; Negative Cache TTL
-                       IN      NS      ns1
+        subnet 10.0.0.0 netmask 255.255.255.0 {
+          option broadcast-address 10.0.0.255;
+          option routers 10.0.0.254;
+          interface vlan1;
+          range 10.0.0.150 10.0.0.200;
+        }
+      '';
+    };
+    bind = {
+      enable = false;
+      cacheNetworks = ["127.0.0.0/24" "10.0.0.0/24"];
+      zones = {
+        "lan" = {
+          master = true;
+          file = pkgs.writeText "lan" ''
+            $ORIGIN lan.
+            $TTL    1h
+            @            IN      SOA     ns1 hostmaster (
+                                             1    ; Serial
+                                             3h   ; Refresh
+                                             1h   ; Retry
+                                             1w   ; Expire
+                                             1h)  ; Negative Cache TTL
+                         IN      NS      ns1
 
-          ns1          IN      A       10.0.0.30
-        '';};
-      "infra.lan" = {
-        master = true;
-        file = pkgs.writeText "infra.lan" ''
-          $ORIGIN infra.lan.
-          $TTL    1h
-          @            IN      SOA     ns1 hostmaster (
-                                           1    ; Serial
-                                           3h   ; Refresh
-                                           1h   ; Retry
-                                           1w   ; Expire
-                                           1h)  ; Negative Cache TTL
-                       IN      NS      ns1
+            ns1          IN      A       10.0.0.30
+          '';
+        };
+        "infra.lan" = {
+          master = true;
+          file = pkgs.writeText "infra.lan" ''
+            $ORIGIN infra.lan.
+            $TTL    1h
+            @            IN      SOA     ns1 hostmaster (
+                                             1    ; Serial
+                                             3h   ; Refresh
+                                             1h   ; Retry
+                                             1w   ; Expire
+                                             1h)  ; Negative Cache TTL
+                         IN      NS      ns1
 
-          ns1          IN      A       10.0.0.30
-          firmware     IN      A       10.0.0.30
-        '';};
+            ns1          IN      A       10.0.0.30
+            firmware     IN      A       10.0.0.30
+          '';
+        };
+      };
     };
   };
 
