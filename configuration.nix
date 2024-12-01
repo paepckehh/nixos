5e5910dfcfb0710bab0dd3781d64fd7f46830f63
PAEPCKE, Michael <git@paepcke.de>
2024-12-01 07:56:05 +0000

    update

--- configuration.nix
+++ configuration.nix
@@ -143,11 +143,7 @@
     timeZone = null; # UTC, local: "Europe/Berlin";
     hardwareClockInLocalTime = true;
   };
-  powerManagement = {
-    enable = true;
-    powertop.enable = false;
-    cpuFreqGovernor = "balanced";
-  };
+  powerManagement.enable = true;
   console = {
     earlySetup = lib.mkForce true;
     keyMap = "us";
@@ -410,6 +406,17 @@
     logind.hibernateKey = "ignore";
     fwupd.enable = true;
     wg-netmanager.enable = true;
+    services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+        START_CHARGE_THRESH_BAT0 = 30;
+        STOP_CHARGE_THRESH_BAT0 = 60;
+      };
+    };
     fstrim = {
       enable = true;
       interval = "daily";
