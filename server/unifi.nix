8a83ad2ddecdef11695c60467b8140b4503b4310
PAEPCKE, Michael <git@paepcke.de>
2024-12-07 21:45:58 +0000

    update

--- server/unifi.nix
+++ server/unifi.nix
@@ -4,6 +4,24 @@
   lib,
   ...
 }: {
+
+  #################
+  #-=# SYSTEMD #=-#
+  #################
+  # workaround for kea
+  systemd.networking.enable = false; 
+
+  ####################
+  #-=# NETWORKING #=-#
+  ####################
+  networking = {
+    firewall = {
+      enable = lib.mkForce false;
+      allowedUDPPorts = [53 67 68];
+      allowedTCPPorts = [53];
+    };
+  };
+ 
   ##################
   #-=# SERVICES #=-#
   ##################
@@ -16,53 +34,42 @@
       exporters = {
         unpoller = {
           enable = false;
-          controllers = [
-            {
+          controllers = [{
               url = "http://localhost:8443";
               user = "readonly";
               pass = "/etc/nixos/server/resources/unifi.txt";
-            }
-          ];
+              }];
+        };
+        kea = {
+          enable = false;
+          targets = [
+            "/run/kea/kea-dhcp4.socket"
+            "http://10.0.0.2"
+          };
         };
       };
-    };
-  };
-
-  ####################
-  #-=# NETWORKING #=-#
-  ####################
-  networking = {
-    firewall = {
-      enable = lib.mkForce false;
-      allowedUDPPorts = [53 67 68];
-      allowedTCPPorts = [53];
-    };
-  };
-
-  ##################
-  #-=# SERVICES #=-#
-  ##################
-  services = {
     kea.dhcp4 = {
       enable = true;
       settings = {
+        authorative = true;
+        rebind-timer = 2000;
+        renew-timer = 1000;
+        valid-lifetime = 4000;
         interfaces-config = {
           interfaces = ["eth0/10.0.0.30"];
-          dhcp-socket-type = "udp";
+          dhcp-socket-type = "raw";
         };
         lease-database = {
           name = "/var/lib/kea/dhcp4.leases";
           persist = true;
           type = "memfile";
         };
-        rebind-timer = 2000;
-        renew-timer = 1000;
-        valid-lifetime = 4000;
         subnet4 = [
           {
             id = 1;
             subnet = "10.0.0.0/24";
             pools = [{pool = "10.0.0.220 - 10.0.0.240";}];
+            comment = "network internal infrastructure pool";
           }
         ];
       };
@@ -96,13 +103,14 @@
           file = pkgs.writeText "infra.lan" ''
             $ORIGIN infra.lan.
             $TTL    1h
-            @            IN      SOA     ns hostmaster (
+            @            IN      SOA     ns1 hostmaster (
                                              1    ; Serial
                                              3h   ; Refresh
                                              1h   ; Retry
                                              1w   ; Expire
                                              1h)  ; Negative Cache TTL
-                         IN      NS      ns
+                         IN      NS      ns1
+                         IN      NS      ns2
             ns1          IN      A       10.0.0.2
             ns2          IN      A       10.0.0.3
             unifi        IN      A       10.0.0.30
