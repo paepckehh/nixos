98dc1da0f08b4ebc5234a923f1613f3187494c83
PAEPCKE, Michael <git@paepcke.de>
2024-12-08 10:01:33 +0000

    update

--- server/unifi/prometheus.nix
+++ server/unifi/prometheus.nix
@@ -38,21 +38,21 @@
           enable = true;
           devices = ["/dev/sda"]; # /dev/nvme0
         };
-        scrapeConfigs = [
-          {
-            job_name = "node";
-            static_configs = [{targets = ["127.0.0.1:${toString config.services.prometheus.exporters.node.port}"];}];
-          }
-          {
-            job_name = "smartctl";
-            static_configs = [{targets = ["localhost:9633"];}];
-          }
-          {
-            job_name = "unpoller";
-            static_configs = [{targets = ["localhost:9130"];}];
-          }
-        ];
       };
+      scrapeConfigs = [
+        {
+          job_name = "node";
+          static_configs = [{targets = ["127.0.0.1:${toString config.services.prometheus.exporters.node.port}"];}];
+        }
+        {
+          job_name = "smartctl";
+          static_configs = [{targets = ["localhost:9633"];}];
+        }
+        {
+          job_name = "unpoller";
+          static_configs = [{targets = ["localhost:9130"];}];
+        }
+      ];
     };
   };
 }
