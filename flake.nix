490c279d4d8273b081eccc29f67831f5b93b9d07
PAEPCKE, Michael <git@paepcke.de>
2024-12-09 11:15:06 +0000

    update

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
           ./server/adguard.nix
           ./server/chronyPublic.nix
           ./server/unifi.nix
-          ./server/virtual.nix
+          # ./server/virtual.nix
           # ./server/firefox-sync-server.nix
           # ./infra/local.nix
           # ./modules/autoupdate.nix
