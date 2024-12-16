e7d37a08f56fb8eb6962f47c3a5d72551348276a
PAEPCKE, Michael <git@paepcke.de>
2024-12-16 06:29:22 +0000

    update

--- server/plik.nix
+++ server/plik.nix
@@ -6,7 +6,24 @@
     plikd = {
       enable = true;
       openFirewall = true;
-      settings = {};
+      settings = {
+        AbuseContac = "it@pvz.digital";
+        MaxFileSizeStr = "4GB";
+        MaxUserSizeStr = "4GB";
+        DefaultTTLStr = "1d";
+        MaxTTLStr = "5d";
+        FeatureAuthentication = "disabled";
+        FeatureOneShot = "enabled"; # Upload with files that are automatically deleted after the first download
+        FeatureRemovable = "disabled"; # Upload with files that anybody can delete
+        FeatureStream = "false"; # Upload with files that are not stored on the server
+        FeaturePassword = "enabled"; # Upload that are protected by HTTP basic auth login/password
+        FeatureComments = "disabled"; # Upload with markdown comments / forced -> default
+        FeatureSetTTL = "enabled"; # When disabled upload TTL is always set to DefaultTTL
+        FeatureExtendTTL = "disabled"; # Extend upload expiration date by TTL each time it is accessed
+        FeatureClients = "enabled"; # Display the clients download button in the web UI
+        FeatureGithub = "disabled"; # Display the source code link in the web UI
+        FeatureText = "disabled"; # Upload text dialog
+      };
     };
   };
 
