c47a43828ea5b1a3c3f8d548474acf72d7521694
PAEPCKE, Michael <git@paepcke.de>
2025-01-13 11:09:06 +0000

    update

--- server/cgit.nix
+++ server/cgit.nix
@@ -14,20 +14,23 @@
   #-=# SERVICES #=-#
   ##################
   services = {
-    lighttpd.cgit = {
+    lighttpd = {
       enable = true;
-      configText = ''
-        enable-commit-graph=1
-        enable-follow-links=1
-        enable-http-clone=1
-        enable-index-links=1
-        enable-remote-branches=1
-        clone-url=http://git.localnet/$CGIT_REPO_URL
-        remove-suffix=1
-        root-title=http://git.localnet
-        root-desc=local git repo store path: /var/repo
-        scanPath=/var/repo
-        snapshots=all'';
+      cgit = {
+        enable = true;
+        configText = ''
+          enable-commit-graph=1
+          enable-follow-links=1
+          enable-http-clone=1
+          enable-index-links=1
+          enable-remote-branches=1
+          clone-url=http://git.localnet/$CGIT_REPO_URL
+          remove-suffix=1
+          root-title=http://git.localnet
+          root-desc=local git repo store path: /var/repo
+          scanPath=/var/repo
+          snapshots=all'';
+      };
     };
   };
 }
