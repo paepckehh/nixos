b4d1a4180503d17345edaa2964d4aa79ce822125
PAEPCKE, Michael <git@paepcke.de>
2025-01-17 07:33:04 +0000

    update

--- server/opnborg-systemd.nix
+++ server/opnborg-systemd.nix
@@ -55,7 +55,7 @@
           "AF_UNIX"
         ];
       };
-      environment = ''
+      environment = {
         "OPN_TARGETS" = "opn01.lan";
         "OPN_APIKEY" = "+RIb6YWNdcDWMMM7W5ZYDkUvP4qx6e1r7e/Lg/Uh3aBH+veuWfKc7UvEELH/lajWtNxkOaOPjWR8uMcD";
         "OPN_APISECRET" = "8VbjM3HKKqQW2ozOe5PTicMXOBVi9jZTSPCGfGrHp8rW6m+TeTxHyZyAI1GjERbuzjmz6jK/usMCWR/p";
@@ -67,11 +67,11 @@
         "OPN_TARGETS_PRODUCTION" = "opn01.lan:8443,opn02.lan:8443";
         "OPN_TARGETS_IMGURL_HOTSTANDBY" = "https://icon-library.com/images/freebsd-icon/freebsd-icon-16.jpg";
         "OPN_TARGETS_IMGURL_PRODUCTION" = "https://icon-library.com/images/freebsd-icon/freebsd-icon-16.jpg";
-        "OPN_SLEEP" = "3600"; # seconds
+        "OPN_SLEEP" = "3600";
         "OPN_DEBUG" = "true";
         "OPN_SYNC_PKG" = "true";
         "OPN_HTTPD_ENABLE" = "true";
-        "OPN_HTTPD_SERVER" = "127.0.0.1:6464"; # port must be above 1024
+        "OPN_HTTPD_SERVER" = "127.0.0.1:6464";
         "OPN_HTTPD_COLOR_FG" = "white";
         "OPN_HTTPD_COLOR_BG" = "grey";
         "OPN_RSYSLOG_ENABLE" = "true";
@@ -81,7 +81,7 @@
         "OPN_GRAFANA_DASHBOARD_HAPROXY" = "rEqu1u5ue/haproxy-2-full";
         "OPN_WAZUH_WEBUI" = "http://localhost:9292";
         "OPN_PROMETHEUS_WEBUI" = "http://localhost:9191";
-      '';
+      };
     };
   };
 }
