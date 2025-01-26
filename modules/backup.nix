e67083a14ea1c859cb69852253f0702d44382fa4
PAEPCKE, Michael <git@paepcke.de>
2025-01-26 19:53:15 +0000

    update

--- modules/backup.nix
+++ modules/backup.nix
@@ -10,18 +10,23 @@
   environment = {
     shellAliases = {
       "backup.home" = ''
-        env sudo -v && cd &&\
+        env sudo -v && cd\
+        export REPO_ROOT="/home" &&\
+        export REPO_OWNER="backup" &&\
+        export REPO_GROUP="backup" &&\
+        export REPO_PATH="$REPO_ROOT/$REPO_OWNER/home" &&\
         export DTS=$(date '+%Y-%m-%d-%H-%M') &&\
-        export BPATH=/home/backup/home &&\
         export FILE=$USER-$DTS.tgz &&\
         export LINK=$USER-current.tgz &&\
-        sudo mkdir $BPATH &&\
+        sudo mkdir -p $REPO_PATH &&\
+        sudo chown -R $REPO_OWNER:$REPO_GROUP $REPO_ROOT &&\
+        sudo chmod -R g=rwX $REPO_ROOT &&\
         echo "[BACKUP.HOME] Performing backup a full backup of $PWD to $BPATH/$FILE" &&\
-        cd && sudo tar -cf - . | sudo zstd --compress -4 --exclude-compressed --auto-threads=physical --threads=0 -o $BPATH/$FILE.tgz &&\
+        cd && sudo tar -cf - . | sudo zstd --compress -4 --exclude-compressed --auto-threads=physical --threads=0 -o $REPO_PATH/$FILE.tgz &&\
         sudo rm -rf $LINK > /dev/null 2>&1 &&\
         sudo ln -fs $FILE $LINK &&\
-        sudo chown -R backup:backup $BPATH
-        sudo chmod -R g=rwX $BPATH'';
+        sudo chown -R $REPO_OWNER:$REPO_GROUP $REPO_ROOT &&\
+        sudo chmod -R g=rwX $REPO_ROOT'';
       "restore.home" = ''
         env sudo -v &&\
         export LINK=$USER-current.tgz &&\
@@ -35,50 +40,41 @@
     };
     etc."gitops.sh".text = lib.mkForce ''
       #!/bin/sh
-      REPO_PATH="/home/backup/repos"
-      REPO_OWNER="backup"
-      REPO_GROUP="backup"
+      export REPO_ROOT="/home"
+      export REPO_OWNER="backup"
+      export REPO_GROUP="backup"
+      export REPO_PATH="$REPO_ROOT/$REPO_OWNER/repos"
+      sudo -v
+      sudo mkdir -p $REPO_PATH
+      sudo chown -R $REPO_OWNER:$REPO_GROUP $REPO_ROOT
+      sudo chmod -R g=rwX $REPO_ROOT
 
       action() {
       	echo "### $XCMD"
       	sudo -u $REPO_OWNER $XCMD
       }
 
-      sudo -v
-      sudo mkdir -p $REPO_PATH
-      sudo chown -R $REPO_OWNER:$REPO_GROUP $REPO_PATH
-      sudo chmod -R g=rwX $REPO_PATH
-
-      	ls $REPO_PATH | while read target; do
-      		FOLDER=$REPO_PATH/$target
-      		if [ ! -d $FOLDER ]; then continue; fi
-      			ls $FOLDER | while read sub; do
-      				REPO=$FOLDER/$sub
-      				if [ ! -d $REPO ]; then continue; fi
-      					if [ ! -d $REPO/.git ]; then continue; fi
-      					echo "############################################################"
-      					echo "$REPO" | sudo -u $REPO_OWNER tee $REPO/.git/description
-      					case $1 in
-      					fetch) XCMD="git -C $REPO fetch --all --force" && action && XCMD="git gc --auto" && action ;;
-      					pull) XCMD="git -C $REPO pull --all --force" && action && XCMD="git gc --auto" && action ;;
-      					compact) XCMD="git -C $REPO gc --aggressive" && action ;;
-      					repair) XCMD="sudo -C $REPO git fsck" && action ;;
-      					update)
-      						if [ -d $REPO/.git ]; then
-      							echo "### git repo mode"
-      							XCMD="sudo git -C $REPO fetch --all --force" && action && XCMD="git -C $REPO gc --auto" && action
-      						else
-      							echo "### git worktree mode"
-      							XCMD="sudo git -C $REPO pull --all --force" && action && XCMD="git -C $REPO gc --auto" && action
-      						fi
-      						;;
-      					*) echo "Please choose one of the following actions: [update|compact|repair|fetch|pull]" ;;
-      					esac
-      			done
+      ls $REPO_PATH | while read target; do
+      	FOLDER=$REPO_PATH/$target
+      	if [ ! -d $FOLDER ]; then continue; fi
+      	ls $FOLDER | while read sub; do
+      		REPO=$FOLDER/$sub
+      		if [ ! -d $REPO ]; then continue; fi
+      		if [ ! -d $REPO/.git ]; then continue; fi
+      		echo "############################################################"
+      		echo "$REPO" | sudo -u $REPO_OWNER tee $REPO/.git/description
+      		case $1 in
+      		fetch) XCMD="git -C $REPO fetch --all --force" && action && XCMD="git gc --auto" && action ;;
+      		pull) XCMD="git -C $REPO pull --all --force" && action && XCMD="git gc --auto" && action ;;
+      		compact) XCMD="git -C $REPO gc --aggressive" && action ;;
+      		repair) XCMD="sudo -C $REPO git fsck" && action ;;
+      		update) XCMD="sudo git -C $REPO pull --all --force" && action && XCMD="git -C $REPO gc --auto" && action ;;
+      		*) echo "Please choose one of the following actions: [update|compact|repair|fetch|pull]" ;;
+      		esac
       	done
-      sudo chown -R $REPO_OWNER:$REPO_GROUP $REPO_PATH
-      sudo chmod -R g=rwX $REPO_PATH
-    '';
+      done
+      sudo chown -R $REPO_OWNER:$REPO_GROUP $REPO_ROOT
+      sudo chmod -R g=rwX $REPO_ROOT'';
   };
 
   ###############
--- modules/resources/gitops.sh
+++ modules/resources/gitops.sh
@@ -1,48 +1,36 @@
 #!/bin/sh
-REPO_PATH="/home/backup/repo"
-REPO_OWNER="backup"
-REPO_GROUP="backup"
+export REPO_ROOT="/home"
+export REPO_OWNER="backup"
+export REPO_GROUP="backup"
+export REPO_PATH="$REPO_ROOT/$REPO_OWNER/repos"
+sudo -v
+sudo mkdir -p $REPO_PATH
+sudo chown -R $REPO_OWNER:$REPO_GROUP $REPO_ROOT
+sudo chmod -R g=rwX $REPO_ROOT
 
 action() {
 	echo "### $XCMD"
 	sudo -u $REPO_OWNER $XCMD
 }
 
-sudo -v
-sudo mkdir -p $REPO_PATH
-sudo chown -R $REPO_OWNER:$REPO_GROUP $REPO_PATH
-
-cd $REPO_PATH && {
-	ls | while read target; do
-		FOLDER=$REPO_PATH/$target
-		if [ ! -d $FOLDER ]; then continue; fi
-		cd $FOLDER && {
-			ls | while read sub; do
-				REPO=$FOLDER/$sub
-				if [ ! -d $REPO ]; then continue; fi
-				cd $REPO && {
-					if [ ! -d .git ]; then continue; fi
-					echo "############################################################"
-					echo "$REPO" | sudo -u cgit tee .git/description
-					case $1 in
-					fetch) XCMD="git fetch --all --force" && action && XCMD="git gc --auto" && action ;;
-					pull) XCMD="git pull --all --force" && action && XCMD="git gc --auto" && action ;;
-					compact) XCMD="git gc --aggressive" && action ;;
-					repair) XCMD="sudo git fsck" && action ;;
-					update)
-						if [ -d .git ]; then
-							echo "### git repo mode"
-							XCMD="sudo git fetch --all --force" && action && XCMD="git gc --auto" && action
-						else
-							echo "### git worktree mode"
-							XCMD="sudo git pull --all --force" && action && XCMD="git gc --auto" && action
-						fi
-						;;
-					*) echo "Please choose one of the following actions: [update|compact|repair|fetch|pull]" ;;
-					esac
-				}
-			done
-		}
+ls $REPO_PATH | while read target; do
+	FOLDER=$REPO_PATH/$target
+	if [ ! -d $FOLDER ]; then continue; fi
+	ls $FOLDER | while read sub; do
+		REPO=$FOLDER/$sub
+		if [ ! -d $REPO ]; then continue; fi
+		if [ ! -d $REPO/.git ]; then continue; fi
+		echo "############################################################"
+		echo "$REPO" | sudo -u $REPO_OWNER tee $REPO/.git/description
+		case $1 in
+		fetch) XCMD="git -C $REPO fetch --all --force" && action && XCMD="git gc --auto" && action ;;
+		pull) XCMD="git -C $REPO pull --all --force" && action && XCMD="git gc --auto" && action ;;
+		compact) XCMD="git -C $REPO gc --aggressive" && action ;;
+		repair) XCMD="sudo -C $REPO git fsck" && action ;;
+		update) XCMD="sudo git -C $REPO pull --all --force" && action && XCMD="git -C $REPO gc --auto" && action ;;
+		*) echo "Please choose one of the following actions: [update|compact|repair|fetch|pull]" ;;
+		esac
 	done
-}
-sudo chown -R $REPO_OWNER:$REPO_GROUP $PATH
+done
+sudo chown -R $REPO_OWNER:$REPO_GROUP $REPO_ROOT
+sudo chmod -R g=rwX $REPO_ROOT
