b83bf68e607bd91c70f7898e886d381bc635baa0
PAEPCKE, Michael <git@paepcke.de>
2025-02-04 09:46:19 +0000

    final: wazuh is up and running

--- server/wazuh.nix
+++ server/wazuh.nix
@@ -23,7 +23,7 @@ with lib; let
   #######################
   wazuh = {
     enabled = true;
-    autostart = false;
+    autostart = true;
     version = "4.10.1";
     webui = {
       dashboard = {
