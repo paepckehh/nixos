47b99db293a8db91e05e5d515dab3a453463a37a
PAEPCKE, Michael <git@paepcke.de>
2025-02-10 11:35:23 +0000

    update

--- server/mongodb.nix
+++ server/mongodb.nix
@@ -3,6 +3,7 @@
   pkgs,
   ...
 }: {
+
   #####################
   #-=# ENVIRONMENT #=-#
   #####################
@@ -23,13 +24,13 @@
            ipv6: false
            unixDomainSocket:
               enabled: true
-              filePermissions: 0700
+              filePermissions: 0770
               pathPrefix: /tmp
         storage:
            engine: wiredTiger
            directoryPerDB: true
-           syncPeriodSecs: 60
-           journal.commitIntervalMs: 500
+           syncPeriodSecs: 120
+           journal.commitIntervalMs: 2000
         systemLog:
            timeStampFormat: iso8601-utc
            verbosity: 0
@@ -43,19 +44,40 @@
       user = "mongodb";
     };
     prometheus = {
-      enable = false;
+      enable = false; # enable everything (grafana, prometheus, exporters, scrapper, ...)
       port = 9191;
-      retentionTime = "60d";
+      retentionTime = "90d";
       alertmanager.port = 9292;
       scrapeConfigs = [
+        {
+          job_name = "node";
+          static_configs = [{targets = ["${config.services.prometheus.exporterts.node.listenAddress}:${toString config.services.prometheus.exporters.node.port}"];}];
+        }
+        {
+          job_name = "smartctl";
+          static_configs = [{targets = ["${config.services.prometheus.exporterts.smartctl.listenAddress}:${toString config.services.prometheus.exporters.smartctl.port}"];}];
+        }
         {
           job_name = "mongodb";
           static_configs = [{targets = ["${config.services.prometheus.exporterts.mongodb.listenAddress}:${toString config.services.prometheus.exporters.mongodb.port}"];}];
         }
       ];
       exporters = {
+        node = {
+          enable = config.services.prometheus.enable;
+          enabledCollectors = ["logind" "systemd"];
+          disabledCollectors = [];
+          listenAddress = "127.0.0.1";
+          port = 9100;
+        };
+        smartctl = {
+          enable = config.services.prometheus.enable;
+          devices = ["/dev/sda"]; # /dev/nvme0
+          listenAddress = "127.0.0.1";
+          port = 9633;
+        };
         mongodb = {
-          enable = false;
+          enable = config.services.prometheus.enable;
           collStats = [];
           collectAll = true;
           collector = [];
@@ -64,13 +86,22 @@
           firewallRules = null;
           group = "mongodb-exporter";
           indexStats = [];
-          listenAddress = "127.0.0.1";
           openFirewall = false;
-          port = "9216";
           telemetryPath = "/metrics";
           uri = "mongodb://127.0.0.1:27017/db";
           user = "mongodb-exporter";
+          listenAddress = "127.0.0.1";
+          port = 9216;
         };
+      };
+    };
+    grafana = {
+      enable = config.services.prometheus.enable;
+      provision.enable = config.services.grafana.enable;
+      settings.server = {
+        http_addr = "127.0.0.1";
+        http_port = 9090;
+        domain = "localhost";
       };
     };
   };
