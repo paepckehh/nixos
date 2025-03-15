55586917eae7ac94153e14a1e4e81d9c94754dd2
PAEPCKE, Michael <git@paepcke.de>
2025-03-15 05:46:58 +0000

    update

--- user/dev-env-go.nix
+++ user/dev-env-go.nix
@@ -3,8 +3,7 @@
   pkgs,
   lib,
   ...
-}:
-{
+}: {
   #####################
   #-=# ENVIRONMENT #=-#
   #####################
@@ -32,75 +31,5 @@
       colorschemes.catppuccin.enable = true;
       plugins.lualine.enable = true;
     };
-  };
-}
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
-  #####################
-  #-=# ENVIRONMENT #=-#
-  #####################
-  environment = {
-    systemPackages = with pkgs; [
-      asn
-      bandwhich
-      bmon
-      dmidecode
-      dnstracer
-      dnsutils
-      dust
-      fastfetch
-      fd
-      gnumake
-      gping
-      gh
-      inetutils
-      jq
-      keepassxc
-      kmon
-      moreutils
-      ncdu
-      netscanner
-      nixfmt-rfc-style
-      nix-init
-      nix-output-monitor
-      nixpkgs-review
-      nix-prefetch-scripts
-      nix-search-cli
-      nix-top
-      nix-tree
-      nvme-cli
-      onefetch
-      openssl
-      p7zip
-      paper-age
-      parted
-      passage
-      pciutils
-      progress
-      pv
-      pwgen
-      stress
-      s-tui
-      sysz
-      tcping-go
-      termshark
-      tldr
-      tlsinfo
-      tree
-      trippy
-      tshark
-      tz
-      unzip
-      usbutils
-      ventoy-full
-      xh
-      yamlfmt
-      yq
-      yubikey-manager
-    ];
   };
 }
