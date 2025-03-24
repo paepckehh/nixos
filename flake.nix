c23413b40a9225672080085cc33e5139cc4745eb
PAEPCKE, Michael <git@paepcke.de>
2025-03-24 05:48:31 +0000

    update

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "nixos infra";
   inputs = {
     # ONLINE URLs
-    # nixpkgs.url = "github:paepckehh/nixpkgs/blocky-improve";
+    nixpkgs.url = "github:paepckehh/nixpkgs/blocky-improve";
     # nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     # dns.url = "github:nix-community/dns.nix/master";
     # nixvim.url = "github:nix-community/nixvim/master";
     nvf.url = "github:notashelf/nvf";
     disko.url = "github:nix-community/disko/master";
     home-manager.url = "github:nix-community/home-manager/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     # settings
     # dns.inputs.nixpkgs.follows = "nixpkgs";
