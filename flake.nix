44b3c869444a554cfd133d6c73531d903276b207
PAEPCKE, Michael <git@paepcke.de>
2025-03-30 10:48:10 +0000

    update

--- flake.nix
+++ flake.nix
@@ -102,7 +102,7 @@
           ./server/dns/blocky.nix
           ./server/dns/blocky-add-prometheus.nix
           ./server/dns/blocky-add-query-stats.nix
-          ./server/monitoring/prometheus-exporter.nix
+          # ./server/monitoring/prometheus-exporter.nix
           # ./server/ai/ollama.nix
           # ./server/ai/openweb-ui.nix
           # ./server/db/mysql.nix
