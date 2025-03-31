2085d7ba4d2d76545c819a1d1e0b182d35e4e377
PAEPCKE, Michael <git@paepcke.de>
2025-03-31 09:40:20 +0000

    update

--- iot/tibber.nix
+++ iot/tibber.nix
@@ -1,7 +1,7 @@
 {config, ...}: {
   # => default web interface prometheus  http://localhost:9090
   # => default web interface grafana     http://localhost:3000  (initial user/password = admin/admin)
-  # => import grafana dashboard at your choice:  https://github.com/terjesannum/tibber-exporter
+  # => import grafana dashboard at your choice:  https://github.com/terjesannum/tibber-exporter/tree/master/grafana
   services = {
     prometheus = {
       enable = true;
@@ -11,29 +11,29 @@
         # tibber bearer token
         # replace this generic developer example token with your personal one
         # keep it safe, do not share (via github) with anyone (agenix, sops, ...)
-        scrapeConfigs = [
-          {
-            job_name = "tibber";
-            static_configs = [
-              {
-                targets = ["127.0.0.1:8080"];
-              }
-            ];
-          }
-        ];
       };
-      grafana = {
-        enable = true;
-        provision = {
-          enable = true;
-          datasources.settings.datasources = [
+      scrapeConfigs = [
+        {
+          job_name = "tibber";
+          static_configs = [
             {
-              name = "Prometheus";
-              type = "prometheus";
-              url = "http://${config.services.prometheus.listenAddress}:${toString config.services.prometheus.port}";
+              targets = ["127.0.0.1:8080"];
             }
           ];
-        };
+        }
+      ];
+    };
+    grafana = {
+      enable = true;
+      provision = {
+        enable = true;
+        datasources.settings.datasources = [
+          {
+            name = "Prometheus";
+            type = "prometheus";
+            url = "http://${config.services.prometheus.listenAddress}:${toString config.services.prometheus.port}";
+          }
+        ];
       };
     };
   };
