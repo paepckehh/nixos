7c060de80a44da70b1a087af4b7f871e09ff2d7d
PAEPCKE, Michael <git@paepcke.de>
2025-04-02 08:03:25 +0000

    update

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    nixpkgs-dev.url = "github:paepckehh/nixpkgs/prometheus-tibber-module";
+    nixpkgs-dev.url = "github:paepckehh/nixpkgs/prometheus-ecoflow-module";
     nixpkgs-unstable.url = "github:paepckehh/nixpkgs/nixos-unstable";
   };
   outputs = {
@@ -102,7 +102,7 @@
           ./server/dns/blocky.nix
           ./server/dns/blocky-add-prometheus.nix
           ./server/dns/blocky-add-query-stats.nix
-          ./iot/tibber.nix
+          ./iot/ecoflow.nix
           # ./server/monitoring/prometheus-exporter.nix
           # ./server/ai/ollama.nix
           # ./server/ai/openweb-ui.nix
---
+++ iot/ecoflow.nix
@@ -0,0 +1,36 @@
+{config, ...}: {
+  # => default web interface prometheus  http://localhost:9090
+  # => default web interface grafana     http://localhost:3000  (initial user/password = admin/admin)
+  # => import grafana dashboard at your choice:
+  services = {
+    prometheus = {
+      enable = true;
+      exporters.ecoflow = {
+        enable = true;
+      };
+      scrapeConfigs = [
+        {
+          job_name = "ecoflow";
+          static_configs = [
+            {
+              targets = ["127.0.0.1:9123"];
+            }
+          ];
+        }
+      ];
+    };
+    grafana = {
+      enable = true;
+      provision = {
+        enable = true;
+        datasources.settings.datasources = [
+          {
+            name = "Prometheus";
+            type = "prometheus";
+            url = "http://${config.services.prometheus.listenAddress}:${toString config.services.prometheus.port}";
+          }
+        ];
+      };
+    };
+  };
+}
