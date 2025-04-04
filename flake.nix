4d1bdeb2b005383cfdcdc2495563df39c70ded8a
PAEPCKE, Michael <git@paepcke.de>
2025-04-04 07:22:37 +0000

    update

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    nixpkgs-dev.url = "github:paepckehh/nixpkgs/ecoflow-dev";
+    nixpkgs-dev.url = "github:paepckehh/nixpkgs/ecoflow-module";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
   outputs = {
