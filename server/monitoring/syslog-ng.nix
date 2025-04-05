dbc93c64a7fe07f55bfa0c5cb314fd843f93f2c3
PAEPCKE, Michael <git@paepcke.de>
2025-04-05 06:55:05 +0000

    update

--- server/monitoring/syslog-ng.nix
+++ server/monitoring/syslog-ng.nix
@@ -1,21 +1,67 @@
-{config, pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  #####################
+  #-=# ENVIRONMENT #=-#
+  #####################
+  environment.shellAliases."console" = ''sudo tail -f /var/syslog-ng/console.txt |  bat --force-colorization --language syslog --paging never'';
+
   ##################
   #-=# SERVICES #=-#
   ##################
   services = {
     syslog-ng = {
       enable = true;
-      extraConfig = "";
+      extraConfig = ''
+        options {
+            time-reap(30);
+            mark-freq(10);
+            keep-hostname(yes);
+        };
+        source s_local {
+            system(); internal();
+        };
+        source s_network_rfc3164 {
+                network(
+                        ip("0.0.0.0")
+                        ip-protocol(4)
+                        transport(tcp)
+                        port(514) 
+                        listen-backlog(4096)
+                        log-msg-size(65536) 
+                        so-reuseport(1)
+                );
+        };
+        destination d_logs {
+            file(
+                "/var/syslog-ng/console.txt"
+                owner("root")
+                group("root")
+                perm(0770)
+                );
+            };
+        log {
+            source(s_local); source(s_network_rfc3164); destination(d_logs);
+        };'';
       extraModulePaths = [];
       package = pkgs.syslogng;
-  };
-    ##############
-    # NETWORKING #
-    ##############
-    networking = {
-      firewall = {
-        allowedTCPPorts = [];
-        allowedUDPPorts = [];
-      };
     };
+  };
+  ##############
+  # NETWORKING #
+  ##############
+  networking = {
+    firewall = {
+      allowedTCPPorts = [
+        514 # RFC3164
+        # 601   # RFC5424
+        # 6514  # RFC5424 TLS Port
+      ];
+      allowedUDPPorts = [
+        # 514  # RFC3164
+      ];
+    };
+  };
 }
