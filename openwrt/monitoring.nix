3ee3ed03100a63956f1522af200ff156b0be3dbf
PAEPCKE, Michael <git@paepcke.de>
2025-04-21 10:38:27 +0000

    bump: deps, fix semver tag

--- openwrt/monitoring.nix
+++ openwrt/monitoring.nix
@@ -6,7 +6,8 @@
   ##############
   #-=# INFO #=-#
   ##############
-  # perform on openwrt side (via ssh()
+  #
+  # perform on openwrt [ssh]
   # apk add prometheus-node-exporter-lua
   # apk add prometheus-node-exporter-lua-nat_traffic
   # apk add prometheus-node-exporter-lua-netstat
@@ -15,9 +16,14 @@
   # apk add prometheus-node-exporter-lua-wifi_stations
   # edit vi /etc/config/prometheus-node-exporter-lua: listen interface and port (see below, targets)
   # service prometheus-node-exporter-lua restart
+  #
+  # nixos:
   # => default web interface prometheus  http://localhost:9090
   # => default web interface grafana     http://localhost:3000  (initial user/password = admin/admin)
-  # add dashboard as you like from grafana.com
+  # add dashboard as you like from grafana.com, example: https://grafana.com/grafana/dashboards/11147-openwrt
+  # need more help? https://www.cloudrocket.at/posts/monitor-openwrt-nodes-with-prometheus/
+  # https://github.com/try2codesecure/grafana_dashboards
+  #
   ##################
   #-=# SERVICES #=-#
   ##################
