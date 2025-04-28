6c670816e5283690e507abc3eff2223051d9b5d5
PAEPCKE, Michael <git@paepcke.de>
2025-04-28 08:02:43 +0000

    update

---
+++ storage/flakes/disko-impermanence.nix
@@ -0,0 +1,53 @@
+{lib, ...}: {
+  ###############
+  #-=# DISKO #=-#
+  ###############
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/$DISKO_DEVICE_MAIN"; # autoinstaller dummy target
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
+            };
+            swap = {
+              size = "8G";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+                priority = 50;
+              };
+            };
+            nix = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/nix";
+                mountOptions = ["noatime" "nodiratime" "discard" "commit=30" "nobarrier" "data=writeback" "journal_async_commit"];
+              };
+            };
+          };
+        };
+      };
+    };
+    nodev = {
+      "/" = {
+        fsType = "tmpfs";
+        mountOptions = [
+          "size=80%"
+        ];
+      };
+    };
+  };
+}
