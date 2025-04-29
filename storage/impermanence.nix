81c2c400c81039ff6ac320da83f0b67959719926
PAEPCKE, Michael <git@paepcke.de>
2025-04-29 06:47:51 +0000

    update

--- storage/impermanence.nix
+++ storage/impermanence.nix
@@ -11,7 +11,7 @@
   # options = lib.mkForce ["mode=755" "noatime" "nodiratime" "discard" "commit=10" "nobarrier" "data=writeback" "journal_async_commit"];
   fileSystems = lib.mkForce {
     "/" = lib.mkForce {
-      device = "none";
+      device = "tmpfs";
       fsType = "tmpfs";
       options = ["defaults" "mode=755" "size=80%"];
     };
@@ -46,7 +46,7 @@
       options = ["bind"];
     };
     "/nix/var/log" = lib.mkForce {
-      device = "none";
+      device = "tmpfs";
       fsType = "tmpfs";
       options = ["defaults" "mode=755" "size=80%"];
     };
