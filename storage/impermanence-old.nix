2345ac70487e1300a1801edd588d735def4d4263
PAEPCKE, Michael <git@paepcke.de>
2025-04-29 08:29:46 +0000

    update

---
+++ storage/impermanence-old.nix
@@ -0,0 +1,49 @@
+{lib, ...}: {
+  #################
+  #-=# IMPORTS #=-#
+  #################
+  # imports = [ ./flakes/disko-impermanence.nix];
+
+  #####################
+  #-=# FILESYSTEMS #=-#
+  #####################
+  # options = ["defaults" "mode=755" "size=80%" "huge=within_size"];
+  # options = lib.mkForce ["mode=755" "noatime" "nodiratime" "discard" "commit=10" "nobarrier" "data=writeback" "journal_async_commit"];
+  fileSystems = lib.mkForce {
+    "/" = lib.mkForce {
+      device = "none";
+      fsType = "tmpfs";
+      options = ["defaults" "mode=755" "size=80%"];
+    };
+    "/boot" = lib.mkForce {
+      device = "/dev/disk/by-partlabel/disk-main-ESP";
+      fsType = "vfat";
+      options = ["fmask=0077" "dmask=0077" "defaults"];
+    };
+    "/nix" = lib.mkForce {
+      device = lib.mkForce "/dev/disk/by-partlabel/disk-main-nix";
+      fsType = lib.mkForce "ext4";
+      options = lib.mkForce ["noatime" "nodiratime" "discard"];
+    };
+    "/home" = lib.mkForce {
+      device = "/nix/persist/home";
+      fsType = "none";
+      options = ["bind"];
+    };
+    "/var/lib" = lib.mkForce {
+      device = "/nix/persist/var/lib";
+      fsType = "none";
+      options = ["bind"];
+    };
+    "/etc/nixos" = lib.mkForce {
+      device = "/nix/persist/etc/nixos";
+      fsType = "none";
+      options = ["bind"];
+    };
+    "/etc/ssh" = lib.mkForce {
+      device = "/nix/persist/etc/ssh";
+      fsType = "none";
+      options = ["bind"];
+    };
+  };
+}
