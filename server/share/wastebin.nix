ff38ee427d9e58d8978633581c0fb8fcec2fc6b2
PAEPCKE, Michael <git@paepcke.de>
2025-04-30 06:13:09 +0000

    update

--- server/share/wastebin.nix
+++ server/share/wastebin.nix
@@ -11,7 +11,7 @@
         WASTEBIN_MAX_BODY_SIZE = 1024;
         WASTEBIN_HTTP_TIMEOUT = 5;
         WASTEBIN_BASEURL = "http://wastebin.lan";
-        WASTEBIN_ADDRESS_PORT = "192.168.8.1:6680";
+        WASTEBIN_ADDRESS_PORT = "192.168.8.100:6680";
         WASTEBIN_RUST_LOG = "info";
         WASTEBIN_THEME = "gruvbox";
       };
