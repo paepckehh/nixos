6c207fd6f49b3db5bf70c5c28b5161dac33d5a23
PAEPCKE, Michael <git@paepcke.de>
2025-05-01 08:16:38 +0000

    update

--- desktop/cosmic.nix
+++ desktop/cosmic.nix
@@ -11,5 +11,11 @@
   ##################
   services = {
     desktopManager.cosmic.enable = true;
+    displayManager.cosmic-greeter.enable = true;
   };
+
+  ##################
+  #-=# HARDWARE #=-#
+  ##################
+  hardware.system76.enableAll = true;
 }
