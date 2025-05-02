26aaff11a99947ca77f894a669532af286a874e2
PAEPCKE, Michael <git@paepcke.de>
2025-05-02 04:56:24 +0000

    update

--- desktop/kiosk.nix
+++ desktop/kiosk.nix
@@ -22,36 +22,38 @@
     };
   };
 
-  ##################
-  #-=# PROGRAMS #=-#
-  ##################
-  programs = {
-    librewolf = {
-      enable = true;
-      policies = {
-        SanitizeOnShutdown = {
-          Cache = true;
-          Cookies = true;
-          Downloads = true;
-          FormData = true;
-          History = true;
-          Sessions = true;
-          SiteSettings = true;
-          OfflineApps = true;
+  ######################
+  #-=# HOME-MANAGER #=-#
+  ######################
+  home-manager.users.kiosk = {
+    programs = {
+      librewolf = {
+        enable = true;
+        policies = {
+          SanitizeOnShutdown = {
+            Cache = true;
+            Cookies = true;
+            Downloads = true;
+            FormData = true;
+            History = true;
+            Sessions = true;
+            SiteSettings = true;
+            OfflineApps = true;
+          };
         };
-      };
-      settings = {
-        "browser.cache.disk.enable" = false;
-        "browser.compactmode.show" = true;
-        "browser.startup.homepage" = "";
-        "signon.rememberSignons" = false;
-        "privacy.clearOnShutdown.history" = false;
-        "privacy.clearOnShutdown.cookies" = false;
-        "privacy.firstparty.isolate" = true;
-        "privacy.resistFingerprinting" = true;
-        "media.ffmpeg.vaapi.enabled" = true;
-        "network.trr.mode" = 0;
-        "webgl.disabled" = false;
+        settings = {
+          "browser.cache.disk.enable" = false;
+          "browser.compactmode.show" = true;
+          "browser.startup.homepage" = "";
+          "signon.rememberSignons" = false;
+          "privacy.clearOnShutdown.history" = false;
+          "privacy.clearOnShutdown.cookies" = false;
+          "privacy.firstparty.isolate" = true;
+          "privacy.resistFingerprinting" = true;
+          "media.ffmpeg.vaapi.enabled" = true;
+          "network.trr.mode" = 0;
+          "webgl.disabled" = false;
+        };
       };
     };
   };
