16cffd7fe1f3e7954c67a270dc870e7c64fff343
PAEPCKE, Michael <git@paepcke.de>
2025-05-05 09:15:59 +0000

    update

--- flake.nix
+++ flake.nix
@@ -117,7 +117,7 @@
           ./packages/neovim-nvf.nix
           ./packages/netops.nix
           ./openwrt/openwrt.nix
-          {tmp.tmpfsHugeMemoryPages="never"}
+          {tmp.tmpfsHugeMemoryPages = "never";}
           # ./server/share/wastebin.nix
           # ./server/monitoring/collect-journald.nix
           # ./server/monitoring/collect-syslog-ng.nix
