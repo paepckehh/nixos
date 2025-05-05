39a50eb75442ee5e7207132f07abdfc327fa3984
PAEPCKE, Michael <git@paepcke.de>
2025-05-05 05:26:57 +0000

    update

--- storage/impermanence-stateless.nix
+++ storage/impermanence-stateless.nix
@@ -15,28 +15,4 @@
     device = "/dev/disk/by-partlabel/disk-main-nix";
     fsType = "ext4";
   };
-
-  fileSystems."/home" = {
-    device = "/nix/persist/home";
-    fsType = "none";
-    options = ["bind"];
-  };
-
-  fileSystems."/var/lib" = {
-    device = "/nix/persist/var/lib";
-    fsType = "none";
-    options = ["bind"];
-  };
-
-  fileSystems."/etc/nixos" = {
-    device = "/nix/persist/etc/nixos";
-    fsType = "none";
-    options = ["bind"];
-  };
-
-  fileSystems."/etc/ssh" = {
-    device = "/nix/persist/etc/ssh";
-    fsType = "none";
-    options = ["bind"];
-  };
 }
