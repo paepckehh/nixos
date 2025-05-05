33a1e87a146a3e6b080e4146b9915755568df83a
PAEPCKE, Michael <git@paepcke.de>
2025-05-05 11:41:53 +0000

    update

--- configuration.nix
+++ configuration.nix
@@ -151,13 +151,13 @@
         "https://cache.nixos.org"
       ];
       substituters = [
-        "https://nixpkgs-unfree.cachix.org"
-        "https://nix-community.cachix.org"
+        # "https://nixpkgs-unfree.cachix.org"
+        # "https://nix-community.cachix.org"
         "https://cache.nixos.org"
       ];
       trusted-public-keys = [
-        "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        # "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+        # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
     };
