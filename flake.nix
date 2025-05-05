c8bfc760ae21d06c2cd14c394312a634c58ba480
PAEPCKE, Michael <git@paepcke.de>
2025-05-05 20:08:45 +0000

    update

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nvf.url = "github:notashelf/nvf";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-dev.url = "github:paepckehh/nixpkgs/tmpfs";
+    nixpkgs-dev.url = "github:paepckehh/nixpkgs/tmpfs-huge";
   };
   outputs = {
     self,
