bfd3a76fa7ad25c1887ea5f46e35e0a9e4a47d1e
PAEPCKE, Michael <git@paepcke.de>
2025-05-12 08:53:55 +0000

    update

--- user/desktop/me.nix
+++ user/desktop/me.nix
@@ -110,7 +110,7 @@
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
               installation_mode = "force_installed";
             };
-            "keepassxc-team" = {
+            "keepassxc@keepass-team" = {
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/keepassxc-browser/latest.xpi";
               installation_mode = "force_installed";
             };
