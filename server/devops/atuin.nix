05d5f808315de394bcb67720d2af4760474da707
PAEPCKE, Michael <git@paepcke.de>
2025-05-24 07:37:31 +0000

    update

--- server/devops/atuin.nix
+++ server/devops/atuin.nix
@@ -2,7 +2,7 @@
   #################
   #-=# SYSTEMD #=-#
   #################
-  systemd.network.networks."10-lan".addresses = [{Addresss = "192.168.80.250/32";}];
+  systemd.network.networks."10-lan".addresses = [{Address = "192.168.80.250/32";}];
 
   ##################
   #-=# SERVICES #=-#
