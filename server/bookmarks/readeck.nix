894405d7cd8942b359bcc588bc551c6d150d0aca
PAEPCKE, Michael <git@paepcke.de>
2025-05-25 07:19:09 +0000

    update

--- server/bookmarks/readeck.nix
+++ server/bookmarks/readeck.nix
@@ -20,8 +20,18 @@
       enable = true;
       environmentFile = null;
       settings = {
-        main.log_level = "debug";
-        server.port = 9696;
+        main = {
+          log_level = "debug";
+          secret_key = "start";
+          data_directory = "/var/lib/readeck";
+        };
+        server = {
+          host = "127.0.0.1";
+          port = 8000;
+        };
+        database = {
+          source = "sqlite3:/var/lib/readeck.sqlite";
+        };
       };
     };
   };
