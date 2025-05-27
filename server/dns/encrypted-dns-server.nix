b424887de03a3c95c37d5cff02912c18c2961b3b
PAEPCKE, Michael <git@paepcke.de>
2025-05-27 20:25:30 +0000

    update

--- server/dns/encrypted-dns-server.nix
+++ server/dns/encrypted-dns-server.nix
@@ -5,6 +5,14 @@
   services = {
     encrypted-dns-server = {
       enable = true;
+      settings = {
+        listen_addrs = [
+          {
+            local = "127.0.0.1:5443";
+            external = "127.0.0.1:5443";
+          }
+        ];
+      };
     };
   };
 }
