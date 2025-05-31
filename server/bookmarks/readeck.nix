ca1715bd4f0daa7bcc700632742be54d42ba91dc
PAEPCKE, Michael <git@paepcke.de>
2025-05-31 08:08:15 +0000

    update

--- server/bookmarks/readeck.nix
+++ server/bookmarks/readeck.nix
@@ -41,13 +41,17 @@
       logDir = lib.mkForce "/var/log/caddy";
       logFormat = lib.mkForce "level INFO";
       configFile = pkgs.writeText "CaddyfileReadeck" ''
-        read.${config.networking.domain} {
-          tls internal 
-          reverse_proxy ${config.services.readeck.settings.server.host}:${toString config.services.readeck.settings.server.port}
-          @not_intranet { not remote_ip 192.168.80.0/24 }
-          respond @not_intranet 403
-          log { output file /var/log/caddy/proxy-read-access.log }
-        }'';
+             read.${config.networking.domain} {
+        tls internal
+               reverse_proxy ${config.services.readeck.settings.server.host}:${toString config.services.readeck.settings.server.port}
+        @not_intranet {
+         not remote_ip 192.168.80.0/24
+        }
+        respond @not_intranet 403
+        log {
+         output file ${config.services.caddy.logDir}/access/proxy-read.log
+        }
+             }'';
     };
   };
 
