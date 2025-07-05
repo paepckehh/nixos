bbeaa932f7a0ead21649fa293ef3825a73c88146
PAEPCKE, Michael <git@paepcke.de>
2025-07-05 06:53:23 +0000

    update

--- flake.nix
+++ flake.nix
@@ -141,8 +141,7 @@
           ./packages/secops.nix
           ./openwrt/alias.nix
           ./iot/moode/alias.nix
-          ./virtual/docker.nix
-          # ./server/hr/timeoff.nix
+          ./server/hr/timeoff.nix
           # ./server/dns/adguard.nix
           # ./server/pki/small-step.nix
           # ./server/share/wastebin.nix
