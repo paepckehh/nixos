d86d60e6695d97091ac22a7227a3c9f094de01b7
PAEPCKE, Michael <git@paepcke.de>
2025-08-22 07:01:10 +0000

    update

--- server/iam/lldap.nix
+++ server/iam/lldap.nix
@@ -33,13 +33,13 @@
         database_url = "sqlite://./users.db?mode=rwc";
         http_url = "http://localhost:9090/";
         http_host = "127.0.0.1";
-        http_port = "9090";
+        http_port = 9090;
         ldap_base_dn = "dc=debitor,dc=corp";
         ldap_user_dn = "admin";
         ldap_user_email = "it@debitor.de";
         ldap_user_pass_file = config.age.secrets.lldap-admin.path;
         ldap_host = "127.0.0.1";
-        ldap_port = "3890";
+        ldap_port = 3890;
       };
     };
   };
