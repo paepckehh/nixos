280c0e973eb728d8c172775aab3e748d1a560ecf
PAEPCKE, Michael <git@paepcke.de>
2025-08-24 05:15:16 +0000

    update

--- server/iam/lldap.nix
+++ server/iam/lldap.nix
@@ -106,7 +106,7 @@ in {
   #-=# ENVIRONMENT #=-#
   #####################
   environment = {
-    systemPackages = with pkgs; [lldap-cli jxplorer sqlitebrowser];
+    systemPackages = with pkgs; [lldap-cli sqlitebrowser];
     etc."ssl/ldaps.crt".text = lib.mkForce ''
       ### LDAPS PEM CERT GOES HERE ###
     '';
@@ -122,28 +122,36 @@ in {
         # db
         database_url = "sqlite://./users.db?mode=rwc";
         # http web gui
-        http_url = "https://${infra.lan.services.ldap-gui.hostname}.${toString infra.lan.services.ldap-gui.domain}";
         http_host = "${infra.lan.services.ldap-gui.localbind.ip}";
         http_port = infra.lan.services.ldap-gui.localbind.port;
-        # ldap interface
-        ldap_jwt_secret = config.age.secrets.lldap-jwt.path;
-        ldap_key_seed = config.age.secrets.lldap-seed.path;
-        ldap_base_dn = "${infra.lan.services.ldap.base-dn}";
-        ldap_user_dn = "${infra.lan.services.ldap.admin.user}";
-        ldap_user_email = "${infra.lan.services.ldap.admin.email}";
-        ldap_user_pass_file = config.age.secrets.lldap-admin.path;
+        http_url = "https://${infra.lan.services.ldap-gui.hostname}.${toString infra.lan.services.ldap-gui.domain}";
+        # ldap
         ldap_host = "${infra.lan.services.ldap.ip}";
         ldap_port = infra.lan.services.ldap.ports.tcp;
+        ldap_base_dn = "${infra.lan.services.ldap.base-dn}";
         # ldaps
         ldaps_options_enabled = false;
         ldaps_options_cert_file = "/etc/ssl/ldaps.crt";
         ldaps_options_key_file = config.age.secrets.lldap-key.path;
+        # ldap secrets
+        ldap_jwt_secret = config.age.secrets.lldap-jwt.path;
+        ldap_key_seed = config.age.secrets.lldap-seed.path;
+        # ldap admin
+        ldap_user_dn = "${infra.lan.services.ldap.admin.user}";
+        ldap_user_email = "${infra.lan.services.ldap.admin.email}";
+        ldap_user_pass_file = config.age.secrets.lldap-admin.path;
+        force_ldap_user_pass_reset = false; # true for enforced reset to default above
+        # compatiblity (AD)
+        ignored_user_attributes = ["sAMAccountName"];
+        ignored_group_attributes = ["mail" "userPrincipalName"];
         # smtp
-        smtp_options_enable_password_reset = true;
-        smtp_options_server = "smtp.${infra.lan.services.ldap.domain}";
-        smtp_options_port = 25;
-        smtp_options_smtp_encryption = "NONE";
-        smtp_options_from = "${infra.lan.services.ldap.admin.email}";
+        smtp_options = {
+          enable_password_reset = true;
+          server = "smtp.${infra.lan.services.ldap.domain}";
+          port = 25;
+          smtp_encryption = "NONE";
+          from = "${infra.lan.services.ldap.admin.email}";
+        };
       };
     };
   };
