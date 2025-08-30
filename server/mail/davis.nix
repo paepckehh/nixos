431c3fb39697f550dc1be375f6572b110830801b
PAEPCKE, Michael <git@paepcke.de>
2025-08-30 08:46:51 +0000

    update

--- server/mail/davis.nix
+++ server/mail/davis.nix
@@ -7,27 +7,32 @@
     lan = {
       services = {
         ldap = {
-          url = "ldap://${infra.lan.services.ldap.server.host}:${infra.lan.services.ldap.server.port}";
-          server = {
-            host = "10.20.0.126";
-            port = 3890;
-          };
+          ip = "10.20.0.126";
+          port = 3890;
+          hostname = "ldap";
+          domain = "dbt.corp";
+          url = "ldap://${infra.lan.services.ldap.ip}:${infra.lan.services.ldap.port}";
+          fqdn = "${infra.lan.services.ldap.hostname}.${infra.lan.services.ldap.domain}";
+          # url = "ldap://${infra.lan.services.ldap.fqdn}:${infra.lan.services.ldap.port}";
         };
         smtp = {
-          domain = "mail.corp";
-          server = {
-            host = "smtp.mail.corp";
-            port = 25;
-          };
+          ip = "10.20.0.126";
+          port = 25;
+          domain = "dbt.corp";
+          maildomain = "debitor.de";
+          host = "smtp";
+          fqdn = "${infra.lan.services.smtp.hostname}.${infra.lan.services.smtp.domain}";
         };
         caldav = {
           admin = "admin";
           ip = "10.20.6.127";
-          hostname = "cal";
+          hostname = "caldav";
           domain = "dbt.corp";
+          fqdn = "${infra.lan.services.caldav.hostname}.${infra.lan.services.caldav.domain}";
           namespace = "06-dbt";
           network = "10.20.6.0/23";
           ports.tcp = 443;
+          url = "https://${infra.lan.services.caldav.fqdn}";
           localbind = {
             ip = "127.0.0.1";
             ports.tcp = 7127;
@@ -102,17 +107,17 @@ in {
       database.createLocally = true;
       mail = {
         dsn = "smtp://calendar:calendar@${infra.lan.services.smtp.server.host}:${toString infra.lan.services.smtp.server.port}";
-        inviteFromAddress = "calendar@${infra.lan.services.smtp.domain}";
+        inviteFromAddress = "calendar@${infra.lan.services.smtp.maildomain}";
       };
       config = {
         PUBLIC_CALENDARS_ENABLED = true;
-        LDAP_AUTH_URL = "ldap://10.20.0.124:3860";
+        LDAP_AUTH_URL = "${infra.lan.services.ldap.url}";
         LDAP_DN_PATTERN = "mail=%u";
         LDAP_MAIL_ATTRIBUTE = "mail";
         LDAP_AUTH_USER_AUTOCREATE = true;
         LDAP_CERTIFICATE_CHECKING_STRATEGY = "never"; # never, try, hard, demand, allow
       };
-      hostname = "localhost";
+      hostname = "localhost:7127";
       # hostname = "${infra.lan.services.caldav.hostname}.${infra.lan.services.caldav.domain}";
     };
     caddy = {
