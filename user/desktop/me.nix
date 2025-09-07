6f3a2ceedcac8e7492e1815fb77df2b9eec0b5fb
PAEPCKE, Michael <git@paepcke.de>
2025-09-07 08:12:29 +0000

    update

--- user/desktop/me.nix
+++ user/desktop/me.nix
@@ -140,8 +140,8 @@
           {
             default_server_config = {
               "m.homeserver" = {
-                  base_url = "http://localhost:7129";
-                  server_name = "dbt.corp";
+                  base_url = "https://matrix-client.matrix.org";
+                  server_name = "matrix.org";
               };
               "m.identity_server" = {
                   base_url = "https://vector.im";
