9cc69f5e56390d51fe5c0897bf5ac0879df4bceb
PAEPCKE, Michael <git@paepcke.de>
2025-09-07 08:10:32 +0000

    update

--- user/desktop/me.nix
+++ user/desktop/me.nix
@@ -137,23 +137,26 @@
       element-desktop = {
         enable = true;
         profiles = ''
-          {
-            debitor = {
-              disable_custom_urls = false;
-              disable_guests = false;
-              disable_login_language_selector = false;
-              disable_3pid_login = false;
-              force_verification = false;
-              brand = "Debitor Message";
-              default_server_config = {
-                "m.homeserver" = {
-                    base_url = "http://localhost:7129";
-                    server_name = "dbt.corp";
-                };
-              };
-            };
-          }'';
+  {
+    default_server_config = {
+      "m.homeserver" = {
+          base_url = "http://localhost:7129";
+          server_name = "dbt.corp";
       };
+      "m.identity_server" = {
+          base_url = "https://vector.im";
+      };
+    };
+    disable_custom_urls = false;
+    disable_guests = false;
+    disable_login_language_selector = false;
+    disable_3pid_login = false;
+    force_verification = false;
+    brand = "Element";
+    integrations_ui_url = "https://scalar.vector.im/";
+    integrations_rest_url = "https://scalar.vector.im/api";
+  }
+'';
       keepassxc = {
         enable = true;
         settings = {
