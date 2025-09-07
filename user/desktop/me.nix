827cf031a13d559792604059e95db09cf9656982
PAEPCKE, Michael <git@paepcke.de>
2025-09-07 08:06:20 +0000

    update

--- user/desktop/me.nix
+++ user/desktop/me.nix
@@ -136,25 +136,22 @@
       };
       element-desktop = {
         enable = true;
-        settings = ''
+        profiles = ''
           {
-                  disable_custom_urls = true;
-                  disable_guests = true;
-                  disable_login_language_selector = false;
-                  disable_3pid_login = false;
-                  force_verification = false;
-                  brand = "Debitor Internal Message";
-                  embedded_pages.wiki_url = "https://wiki.dbt.corp/matrix/";
-                  branding = {
-                          welcome_background_url = "https://example.org/assets/background.jpg";
-                          auth_header_logo_url =  "https://example.org/assets/logo.svg";
-                  };
-                  default_server_config = {
-                          "m.homeserver" = {
-                          base_url = "https://matrix.dbt.corp";
-                          server_name = "matrix.dbt.corp";
-                          };
-                   };
+            debitor = {
+              disable_custom_urls = false;
+              disable_guests = false;
+              disable_login_language_selector = false;
+              disable_3pid_login = false;
+              force_verification = false;
+              brand = "Debitor Message";
+              default_server_config = {
+                "m.homeserver" = {
+                    base_url = "http://localhost:7129";
+                    server_name = "dbt.corp";
+                };
+              };
+            };
           }'';
       };
       keepassxc = {
