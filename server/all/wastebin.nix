71cb41ae16bd7aa4f1ec49b648eb173b6606806c
PAEPCKE, Michael <git@paepcke.de>
2025-09-12 05:57:25 +0000

    update

--- server/all/wastebin.nix
+++ server/all/wastebin.nix
@@ -2,7 +2,7 @@
   ####################
   #-=# NETWORKING #=-#
   ####################
-  networking.firewall.allowedTCPPorts = [2222];
+  networking.firewall.allowedTCPPorts = [80];
 
   ##################
   #-=# SERVICES #=-#
@@ -11,11 +11,11 @@
     wastebin = {
       enable = true;
       settings = {
-        WASTEBIN_TITLE = "10.20.0.222:2222";
+        WASTEBIN_TITLE = "10.20.0.222:80";
         WASTEBIN_MAX_BODY_SIZE = 10485760;
         WASTEBIN_HTTP_TIMEOUT = 10;
-        WASTEBIN_BASEURL = "http://10.20.0.222:2222";
-        WASTEBIN_ADDRESS_PORT = "10.20.0.222:2222";
+        WASTEBIN_BASEURL = "http://10.20.0.222";
+        WASTEBIN_ADDRESS_PORT = "10.20.0.222:80";
         WASTEBIN_THEME = "monokai";
       };
     };
