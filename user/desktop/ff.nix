dda3018b81db0c143c204d82948664776702bc26
PAEPCKE, Michael <git@paepcke.de>
2025-09-23 04:12:11 +0000

    update

--- user/desktop/ff.nix
+++ user/desktop/ff.nix
@@ -219,6 +219,10 @@
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/readeck/latest.xpi";
             installation_mode = "force_installed";
           };
+          "446900e4-71c2-419f-a6a7-df9c091e268b" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+            installation_mode = "force_installed";
+          };
           #"floccus@floccus.org" = {
           #  install_url = "https://addons.mozilla.org/firefox/downloads/latest/floccus/latest.xpi";
           #  installation_mode = "force_installed";
