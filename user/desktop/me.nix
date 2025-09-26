98535e27bc665dc57730bb73aeac8b02e644d68c
PAEPCKE, Michael <git@paepcke.de>
2025-09-26 03:46:45 +0000

    update

--- user/desktop/me.nix
+++ user/desktop/me.nix
@@ -113,35 +113,8 @@
         installBatSyntax = true;
         installVimSyntax = true;
         settings = {
-          theme = "adm";
+          theme = "Synthwave";
           font-size = 13;
-        };
-        themes = {
-          adm = {
-            background = "000000";
-            foreground = "fffbf6";
-            cursor-color = "f5e0dc";
-            palette = [
-              "0=#45475a"
-              "1=#f38ba8"
-              "2=#a6e3a1"
-              "3=#f9e2af"
-              "4=#89b4fa"
-              "5=#f5c2e7"
-              "6=#94e2d5"
-              "7=#bac2de"
-              "8=#585b70"
-              "9=#f38ba8"
-              "10=#a6e3a1"
-              "11=#f9e2af"
-              "12=#89b4fa"
-              "13=#f5c2e7"
-              "14=#94e2d5"
-              "15=#a6adc8"
-            ];
-            selection-background = "353749";
-            selection-foreground = "cdd6f4";
-          };
         };
       };
       alacritty = {
