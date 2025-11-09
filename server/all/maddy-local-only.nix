c75460181bd2c963dcd3534ae55cc7eb582c6bfe
PAEPCKE, Michael <git@paepcke.de>
2025-11-09 15:56:01 +0000

    update

--- server/all/maddy-local-only.nix
+++ server/all/maddy-local-only.nix
@@ -35,32 +35,18 @@ in {
       hostname = infra.smtp.fqdn;
       primaryDomain = infra.smtp.domain;
       config = ''
-        table.chain local_rewrites {
-          optional_step regexp "(.+)\+(.+)@(.+)" "$1@$3"
-          optional_step file /etc/maddy/aliases
-        }
-        storage.imapsql local_mailboxes {
-          driver sqlite3
-          dsn imapsql.db
-          compression zstd 6
-          debug on
-        }
+        # smtp
         smtp tcp://${infra.smtp.ip}:${toString infra.port.smtp} {
           limits {
             all rate 20 1s
             all concurrency 10
           }
           destination ${infra.smtp.domain} {
-            modify {
-               replace_rcpt &local_rewrites
-            }
-            deliver_to &local_mailboxes
-          }
-          default_destination {
-           reject 550 5.1.1 "User doesn't exist in local target domain, or wrong target domain."
+            deliver_to &local_routing
           }
           debug on
         }
+        # imap
         imap tcp://${infra.imap.ip}:${toString infra.port.imap} {
           auth &local_authdb
           insecure_auth yes
@@ -69,12 +55,46 @@ in {
           io_errors on
           io_debug on
         }
+        # ldap user auth
         auth.ldap local_authdb {
           urls ${infra.ldap.uri}
           dn_template "cn={username},${infra.ldap.baseDN}"
           starttls off
           connect_timeout 10s
           debug on
+        }
+        # store backend
+        storage.imapsql local_mailboxes {
+          driver sqlite3
+          dsn imapsql.db
+          compression zstd 6
+          debug on
+        }
+        # routing
+        msgpipeline local_routing {
+          modify {
+            replace_rcpt &local_rewrites
+          }
+          destination_in &local_mailboxes {
+            deliver_to &local_mailboxes
+          }
+          destination $(local_domains) {
+            modify {
+              replace_rcpt regexp ".*" "catchall@$(primary_domain)"
+            }
+            deliver_to &local_mailboxes
+          }
+          default_destination {
+            reject 550 5.1.1 "User doesn't exist"
+          }
+        }
+        # rewrite filter
+        table.chain local_rewrites {
+          optional_step regexp "(.+)\+(.+)@(.+)" "$1@$3"
+          optional_step static {
+            entry postmaster postmaster@$(primary_domain)
+          }
+          optional_step file /etc/maddy/aliases
         }
       '';
     };
