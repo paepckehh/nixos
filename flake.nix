8d13488ab12e2f7d4ff7fe8091f1491f075e0a4a
PAEPCKE, Michael <git@paepcke.de>
2025-11-17 13:23:12 +0000

    update

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nvf.url = "github:notashelf/nvf";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # nixpkgs.url = "git+file:///home/projects/nixos/nixos_nixpkgs?ref=nixos-unstable";
   };
   outputs = {
