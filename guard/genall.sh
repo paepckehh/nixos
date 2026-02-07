2bc742750067c7d283f8dc35238d348d114967ce
PAEPCKE, Michael <git@paepcke.de>
2026-02-07 11:09:59 +0000

    update

--- guard/genall.sh
+++ guard/genall.sh
@@ -12,38 +12,39 @@ if ! [ $ID -lt 250 ]; then
         echo "The ID variable defines the last octed of the ip address and hostname appendinx."
         exit 1
 fi
+if [ ! -x $(TARGETDIR) ]; then 
+	echo "Unable to access $(TARGETDIR)"
+	exit 1
+fi
 # kickInit /dev/urand with some added entroy
-curl -vvvvvvvIk https://start 2>&1 | sudo tee -a /dev/urandom
-dd if=/dev/urandom of=/dev/null bs=512k count=128 status=progress
+curl -vvvvvvvIk https://start 2>&1 | openssl enc -a  | openssl enc -a | sudo tee -a /dev/urandom
 counter=1
-PSK=""
-echo "PSK: Start pre-shared key-gen"
-while ! [[ "$PSK" =~ ^[a-zA-Z0-9=]+$ ]]; do
-        PSK=$(wg genpsk)
-        # echo "PSK: ... testing generated shared key: $PSK"
-done
-# echo "PSK: Found valid PSK: $PSK"
-# echo "PK: Start private key gen!"
+while [ $counter -lt 1500 ] ; do 
+PSK="" 
+while ! [ "$(echo -n $PSK | cut -c 2)" == "c" ]; do 
+ PSK=""
+  while ! [[ "$PSK" =~ ^[a-zA-Z0-9=]+$ ]]; do
+    PSK=$(wg genpsk) 
+    ((counter++))
+    echo -n '.'
+  done
+echo -n "!"
+done 
 PUB=""
-while ! [ "$(echo -n $PUB | head -c 1)" == "d" ]; do
- PUB=""
- while ! [[ "$PUB" =~ ^[a-zA-Z0-9=]+$ ]]; do
-  PK=""
-  while ! [[ "$PK" =~ ^[a-zA-Z0-9=]+$ ]]; do
+ while ! [ "$(echo -n $PUB | cut -c 2)" == "c" ]; do
+  PUB=""
+  while ! [[ "$PUB" =~ ^[a-zA-Z0-9=]+$ ]]; do
+   PK=""
+   while ! [[ "$PK" =~ ^[a-zA-Z0-9=]+$ ]]; do
          PK=$(wg genpsk)
         ((counter++))
         echo -n '.'
-        #  echo "PK: ... testing generated PK: $PK"
-  done
-  # echo "PK: Found valid PK: $PK"
-  # echo "PUB: Start genrate private keys!"
-  PUB=$(echo $PK | wg pubkey)
-  # echo "PUB: ... testing Public Key: $PUB of Private Key: $PK"
- echo -n '#'
- done 
- # echo "PUB: Found valid PUB: $PUB"
-echo -n "!"
+   done
+   PUB=$(echo $PK | wg pubkey)
+   echo -n '#'
+   done 
+  echo -n "!"
+ done
 done 
-# echo "PUB: Found valid vanity PUB: $PUB"
 echo
-echo "FINAL: Found valid KeyTriple after $counter rounds => PSK: $PSK , PK: $PK , PUB: $PUB"
+echo "FINAL: Found valid KeyTriple after $counter keys generated => PSK: $PSK , PK: $PK , PUB: $PUB"
