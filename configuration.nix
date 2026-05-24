b91f5f72a70e1be728393394b08a3082185bf897
PAEPCKE, Michael <git@paepcke.de>
2026-05-24 10:17:18 +0200

    update
    Signed-off-by: PAEPCKE, Michael <git@paepcke.de>

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@ in {
   boot = {
     blacklistedKernelModules = infra.kernel.blacklist;
     extraModprobeConfig = infra.kernel.modBlacklist;
-    kernelPackages = pkgs.latestKernelPackage;
+    kernelPackages = pkgs.linuxPackages_latest;
     nixStoreMountOpts = lib.mkForce ["ro" "nodev" "nosuid"];
     runSize = "85%";
     supportedFilesystems = infra.kernel.fs.base;
