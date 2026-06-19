a9c79b74f4266a85617394c3e94b9b0bdba986fb
PAEPCKE, Michael <git@paepcke.de>
2026-06-19 07:12:42 +0200

    update
    Signed-off-by: PAEPCKE, Michael <git@paepcke.de>

--- server/all/crush.nix
+++ server/all/crush.nix
@@ -48,7 +48,17 @@ in {
       ####################
       #-=# NETWORKING #=-#
       ####################
-      networking.hostName = infra.crush.hostname;
+      networking = {
+        hostName = infra.crush.hostname;
+        firewall = {
+          allowedUDPPortRanges = [
+            {
+              from = 61001;
+              to = 61999;
+            }
+          ];
+        };
+      };
 
       #####################
       #-=# ENVIRONMENT #=-#
@@ -58,7 +68,41 @@ in {
           tsshd
           crush
         ];
-        etc."ssh/sshd_config".text = "...";
+        etc."ssh/sshd_config".text = ''
+          AddressFamily inet
+          AllowAgentForwarding no
+          AllowUsers me
+          AuthenticationMethods publickey
+          AuthorizedPrincipalsFile none
+          ChallengeResponseAuthentication no
+          Ciphers chacha20-poly1305@openssh.com
+          ClientAliveCountMax 3
+          ClientAliveInterval 30
+          Compression no
+          GatewayPorts no
+          HostKey /etc/ssh/ssh_host_ed25519_key
+          KbdInteractiveAuthentication no
+          KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org
+          LogLevel INFO
+          LoginGraceTime 2m
+          MaxStartups 10:30:100
+          PasswordAuthentication no
+          PerSourceMaxStartups 12
+          PerSourceNetBlockSize 32:128
+          PermitRootLogin no
+          PrintMotd no
+          PubkeyAuthOptions touch-required
+          PubkeyAuthentication yes
+          RekeyLimit 512M, 1h
+          StrictModes yes
+          UseDns no
+          UsePAM no
+          X11Forwarding no
+          AddressFamily inet
+          ListenAddress 10.20.0.100:6623
+          AuthorizedKeysFile /etc/ssh/authorized_keys.d/%u
+          HostKey /etc/ssh/ssh_host_ed25519_key
+        '';
       };
 
       #################
