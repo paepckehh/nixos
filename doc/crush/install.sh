eb2f7c66371cbb28c0f41127a5ba52edb050774b
PAEPCKE, Michael <git@paepcke.de>
2026-07-03 06:38:28 +0200

    update
    Signed-off-by: PAEPCKE, Michael <git@paepcke.de>

--- doc/crush/install.sh
+++ doc/crush/install.sh
@@ -4,6 +4,5 @@ rm -rf ~/.config/crush/skills > /dev/null 2>&1
 rm -rf ~/.local/share/crush   > /dev/null 2>&1 
 mkdir -p ~/.config/crush
 mkdir -p ~/.local/share/crush
-cp -af /etc/nixos/doc/crush/.agents ~/
 cp -af /etc/nixos/doc/crush/skills ~/.config/crush/
 cp -af /etc/nixos/doc/crush/crush.json ~/.local/share/crush/
