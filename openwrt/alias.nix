82eea42c3c0cc177ee4df6c2cd955442a5876884
PAEPCKE, Michael <git@paepcke.de>
2026-08-02 08:10:55 +0200

    update
    Signed-off-by: PAEPCKE, Michael <git@paepcke.de>

--- openwrt/alias.nix
+++ openwrt/alias.nix
@@ -5,25 +5,25 @@
   environment = {
     shellAliases = {
       # base
-      "axt" = "cd /etc/nixos/openwrt && make axt-luci";
-      "dap" = "cd /etc/nixos/openwrt && make dap-luci";
-      "rpi2" = "cd /etc/nixos/openwrt && make rpi2-luci";
-      "b3000" = "cd /etc/nixos/openwrt && make b3000-luci";
+      "axt" = "cd /etc/nixos/openwrt && make axt.luci";
+      "dap" = "cd /etc/nixos/openwrt && make dap.luci";
+      "rpi2" = "cd /etc/nixos/openwrt && make rpi2.luci";
+      "b3000" = "cd /etc/nixos/openwrt && make b3000/luci";
       # btop
-      "axt.btop" = "cd /etc/nixos/openwrt && make axt-btop";
-      "dap.btop" = "cd /etc/nixos/openwrt && make dap-btop";
-      "rpi2.btop" = "cd /etc/nixos/openwrt && make rpi2-btop";
-      "b3000.btop" = "cd /etc/nixos/openwrt && make b3000-btop";
+      "axt.btop" = "cd /etc/nixos/openwrt && make axt.btop";
+      "dap.btop" = "cd /etc/nixos/openwrt && make dap.btop";
+      "rpi2.btop" = "cd /etc/nixos/openwrt && make rpi2.btop";
+      "b3000.btop" = "cd /etc/nixos/openwrt && make b3000.btop";
       # backup
-      "axt.backup" = "cd /etc/nixos/openwrt && make axt-backup";
-      "dap.backup" = "cd /etc/nixos/openwrt && make axt-dap";
-      "rpi2.backup" = "cd /etc/nixos/openwrt && make rpi2-backup";
-      "b3000.backup" = "cd /etc/nixos/openwrt && make b3000-backup";
+      "axt.backup" = "cd /etc/nixos/openwrt && make axt.backup";
+      "dap.backup" = "cd /etc/nixos/openwrt && make axt.dap";
+      "rpi2.backup" = "cd /etc/nixos/openwrt && make rpi2.backup";
+      "b3000.backup" = "cd /etc/nixos/openwrt && make b3000.backup";
       # config
-      "axt.config" = "cd /etc/nixos/openwrt && make axt-config";
-      "dap.config" = "cd /etc/nixos/openwrt && make dap-config";
-      "rpi2.config" = "cd /etc/nixos/openwrt && make rpi2-config";
-      "b3000.config" = "cd /etc/nixos/openwrt && make b3000-config";
+      "axt.config" = "cd /etc/nixos/openwrt && make axt.config";
+      "dap.config" = "cd /etc/nixos/openwrt && make dap.config";
+      "rpi2.config" = "cd /etc/nixos/openwrt && make rpi2.config";
+      "b3000.config" = "cd /etc/nixos/openwrt && make b3000.config";
     };
   };
 }
