cd3d267d909a05d05b100d5a728a8efedbe35375
PAEPCKE, Michael <git@paepcke.de>
2026-08-08 08:52:14 +0200

    update
    Signed-off-by: PAEPCKE, Michael <git@paepcke.de>

--- doc/crush/init.sh
+++ doc/crush/init.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+set -x -e
 mkdir -p ~/.agents/skills ~/.config
 cp -af /etc/nixos/doc/crush/.crushrc ~/
 cp -af /etc/nixos/doc/crush/AGENTS.md ~/.config/AGENTS.md
-mkdir /tmp/skills
-cd /tmp/skills
-git clone --depth 1 https://github.com/anthropics/skills anthropics
-git clone --depth 1 https://github.com/samber/cc-skills-golang cc-skills-golang 
-ls -la 
-echo "pick && migrate skills ~/.agents/skill/ && rm -rf /tmp/skills"
+if [ "$1" == "skills" ]; then
+	mkdir -p /tmp/skills
+	cd /tmp/skills || exit 1
+	git clone --depth 1 https://github.com/anthropics/skills anthropics
+	git clone --depth 1 https://github.com/samber/cc-skills-golang cc-skills-golang
+	ls -la
+	echo "pick && migrate skills ~/.agents/skill/ && rm -rf /tmp/skills"
+fi
--- doc/go/go.init.sh
+++ doc/go/go.init.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
-sudo -v 
-set -v -x 
+#
+if [ -x /nix/persist/cache/go/cache ]; then exit 0; fi
+#
+sudo -v
+set -v -x
 #
 sudo mkdir -p /nix/persist/bin
 sudo chown -R 0:0 /nix/persist/bin
 sudo chmod -R 755 /nix/persist/bin
-# 
+#
 TARGET="/nix/persist/cache/go"
 SUBDIRS="cache go-path path pkg"
-sudo rm -rf $TARGET > /dev/null 2>&1  || true
+sudo rm -rf $TARGET >/dev/null 2>&1 || true
 sudo mkdir -p $TARGET
 sudo chown -R 0:users $TARGET
 sudo chmod -R 644 $TARGET
 sudo chmod 755 $TARGET
 for subdir in $SUBDIRS; do
-    sudo mkdir -p "$TARGET/$subdir"
-    sudo chmod 755 "$TARGET/$subdir"
+	sudo mkdir -p "$TARGET/$subdir"
+	sudo chmod 755 "$TARGET/$subdir"
 done
