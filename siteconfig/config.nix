6dbaf63dc813815b84cdfac1823e191f1c70a2d6
PAEPCKE, Michael <git@paepcke.de>
2026-08-27 13:47:25 +0200

    update

--- siteconfig/config.nix
+++ siteconfig/config.nix
@@ -626,14 +626,6 @@ let
               };
             };
           };
-          "readeck@readeck.com" = {
-            installation_mode = "force_installed";
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/readeck/latest.xpi";
-          };
-          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-            installation_mode = "force_installed";
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-          };
         };
         PrintingEnabled = true;
         Proxy = {
